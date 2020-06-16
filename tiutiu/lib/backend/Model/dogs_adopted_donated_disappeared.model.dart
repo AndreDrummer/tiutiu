d543c049d6c57a9fb1c856284b3dae868b9ee6dc
André <anprofelipe@gmail.com>
2020-06-15 21:58:29 -0300

    Controllers finalizados - SQFlite

--- tiutiu/lib/backend/Model/dogs_adopted_donated_disappeared.model.dart
+++ tiutiu/lib/backend/Model/dogs_adopted_donated_disappeared.model.dart
@@ -1,9 +1,11 @@
 class AdoptedDonatedDisappeared {
+  String id;
   String ownerId;
   String dogId;
 
   toJson() {
     return {
+      'id': id,
       'ownerId': ownerId,
       'dogId': dogId
     };
@@ -11,6 +13,7 @@ class AdoptedDonatedDisappeared {
 
   Map<String, Object> toMap() {
     Map<String, Object> dogAdoptedMap = Map<String, Object>();
+    dogAdoptedMap['id'] = id;
     dogAdoptedMap['ownerId'] = ownerId;
     dogAdoptedMap['dogId'] = dogId;
 
