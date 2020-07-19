c9fda3db8f5a8d595967674be89a3e17a64b3609
André <anprofelipe@gmail.com>
2020-07-19 19:35:05 -0300

    Gerando build com validação de errors de autenticação

--- tiutiu/lib/Widgets/inputText.dart
+++ tiutiu/lib/Widgets/inputText.dart
@@ -1,43 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:tiutiu/Custom/icons.dart';
 
-class InputText extends StatelessWidget {
-
+class InputText extends StatefulWidget {
   final double size;
   final String placeholder;
   final TextEditingController controller;
   final bool multiline;
   final bool isLogin;
   final bool isPassword;
+  bool seePassword;
   final int maxlines;
 
+  InputText(
+      {this.size = 55,
+      this.controller,
+      this.isLogin = false,
+      this.isPassword = false,
+      this.seePassword = false,
+      this.placeholder,
+      this.maxlines = 1,
+      this.multiline = false});
 
-  InputText({
-    this.size = 55,
-    this.controller,
-    this.isLogin = false,
-    this.isPassword = false,
-    this.placeholder,
-    this.maxlines = 1,
-    this.multiline = false
-  });
+  InputText.login(
+      {this.size = 55,
+      this.controller,
+      this.placeholder,
+      this.maxlines = 1,
+      this.multiline = false,
+      this.isPassword = false,
+      this.seePassword = false,
+      this.isLogin = true});
 
-  InputText.login({
-    this.size = 55,
-    this.controller,
-    this.placeholder,
-    this.maxlines = 1,
-    this.multiline = false,
-    this.isPassword = false,
-    this.isLogin = true
-  });
+  @override
+  _InputTextState createState() => _InputTextState();
+}
 
+class _InputTextState extends State<InputText> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: size,
+      height: widget.size,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12.0),
-        color: isLogin ? Colors.white70 : Colors.white,
+        color: widget.isLogin ? Colors.white70 : Colors.white,
         border: Border.all(
           style: BorderStyle.solid,
           color: Colors.white,
@@ -45,29 +50,55 @@ class InputText extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: TextFormField(
-          validator: (value) {
-            if(value.isEmpty) {
-              return 'Preencha corretamente esse campo';
-            }
-            return null;
-          },
-          controller: controller,
-          obscureText: isPassword,
-          maxLines: maxlines,
-          keyboardType: multiline ? TextInputType.multiline : TextInputType.text,     
-          decoration: InputDecoration(            
-            labelText: placeholder,
-            labelStyle: TextStyle(
-              color: isLogin ? Colors.black38 : Theme.of(context).textTheme.headline5.color,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Preencha corretamente esse campo';
+                  }
+                  return null;
+                },
+                controller: widget.controller,
+                obscureText: widget.isPassword && !widget.seePassword,
+                maxLines: widget.maxlines,
+                keyboardType:
+                    widget.multiline ? TextInputType.multiline : TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: widget.placeholder,
+                  labelStyle: TextStyle(
+                    color: widget.isLogin
+                        ? Colors.black38
+                        : Theme.of(context).textTheme.headline5.color,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(style: BorderStyle.none),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(style: BorderStyle.none),
+                  ),
+                ),
+              ),
             ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(style: BorderStyle.none),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(style: BorderStyle.none),
-            ),
-          ),
+            widget.isPassword
+                ? Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: IconButton(
+                      icon: Icon(
+                        widget.seePassword ? CustomIcon.eye : CustomIcon.eye_slash,
+                        color: Colors.grey,
+                        size: 18,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          widget.seePassword = !widget.seePassword;
+                        });
+                      },
+                    ),
+                  )
+                : SizedBox(),
+          ],
         ),
       ),
     );
