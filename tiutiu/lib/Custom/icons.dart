e6496888eb950a2a60a15afb78d9c452285f2016
André <anprofelipe@gmail.com>
2020-07-19 19:35:05 -0300

    Gerando build com validação de errors de autenticação

--- tiutiu/lib/Custom/icons.dart
+++ tiutiu/lib/Custom/icons.dart
@@ -3,7 +3,10 @@ import 'package:flutter/widgets.dart';
 class CustomIcon {
   CustomIcon._();
 
-  static const family = 'CustomIcon';
+  static const _kFontFam = 'CustomIcon';
+  static const _kFontPkg = null;
 
-  static const IconData filter_alt = const IconData(0xe800, fontFamily: family);
+  static const IconData eye = IconData(0xf06e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData eye_slash = IconData(0xf070, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData filter = IconData(0xf0b0, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
