6eeb61875af9a06602ddd5c7aa49d7bd51195a9a
André <anprofelipe@gmail.com>
2020-08-12 10:19:25 -0300

    Alterações de layout

--- tiutiu/lib/Widgets/button.dart
+++ tiutiu/lib/Widgets/button.dart
@@ -10,24 +10,22 @@ class ButtonWide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {    
-    return Container(
-      alignment: Alignment.center,
-      height: 50,
-      width:  isToExpand ? MediaQuery.of(context).size.width : 260,
-      decoration: BoxDecoration(
-        borderRadius: rounded == true ? BorderRadius.circular(25) : null,
-        color: Theme.of(context).primaryColor,
-      ),
-      child: RaisedButton(
-        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        elevation: 0,
-        color: Theme.of(context).primaryColor,
-        onPressed: () => action(),
-        child: Text(
-          text,
-          style: Theme.of(context).textTheme.headline1.copyWith(
-              color: Colors.white, fontSize: 22, fontWeight: FontWeight.w700),
+    return GestureDetector(            
+      onTap: () => action(),      
+      child: Container(        
+        alignment: Alignment.center,
+        height: 50,
+        width:  isToExpand ? MediaQuery.of(context).size.width : 260,
+        decoration: BoxDecoration(
+          borderRadius: rounded == true ? BorderRadius.circular(25) : null,
+          color: Theme.of(context).primaryColor,
         ),
+        child:  Text(
+            text,
+            style: Theme.of(context).textTheme.headline1.copyWith(
+                color: Colors.white, fontSize: 22, fontWeight: FontWeight.w700),
+          ),
+        
       ),
     );
   }
--- tiutiu/lib/Widgets/custom_bottom_navigator_bar.dart
+++ tiutiu/lib/Widgets/custom_bottom_navigator_bar.dart
@@ -34,6 +34,8 @@ class CustomBottomNavigatorBar extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
+                  Icon(Icons.pin_drop),
+                  SizedBox(width: 10),
                   Text(
                     'Perto de você',
                     style: Theme.of(context).textTheme.headline1.copyWith(
