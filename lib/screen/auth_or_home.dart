9d149ce51538a72467e6c2657a93277d94425ac8
André <anprofelipe@gmail.com>
2020-08-28 17:06:07 -0300

    Implementando tela de loading

--- lib/screen/auth_or_home.dart
+++ lib/screen/auth_or_home.dart
@@ -3,18 +3,36 @@ import 'package:provider/provider.dart';
 import 'package:tiutiu/providers/auth2.dart';
 import 'package:tiutiu/screen/auth_screen.dart';
 import 'package:tiutiu/screen/home.dart';
+import 'package:loading_animations/loading_animations.dart';
 
 class AuthOrHome extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return Consumer<Authentication>(
       builder: (context, auth, child) {
         return FutureBuilder(
           future: auth.tryAutoLoginIn(),
           builder: (_, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              // TODO: Criar uma tela personalizada de carregamento.
-              return Center(child: CircularProgressIndicator());
+            if (snapshot.connectionState == ConnectionState.waiting) {              
+              return Scaffold(
+                backgroundColor: Colors.black87,
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      LoadingBouncingGrid.square(
+                        size: 100.0,
+                        backgroundColor:  Theme.of(context).primaryColor,                        
+                      ),
+                      SizedBox(height: 30.0),
+                      Text('Carregando aplicativo...', style: Theme.of(context).textTheme.headline6.copyWith(
+                        color: Colors.white
+                      ))
+                    ],
+                  ),
+                ),
+              );
             } else if (snapshot.hasError) {
               return Center(child: Text('Ocorreu um erro! ${snapshot.error}'));
             } else {
