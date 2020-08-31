6861ffef35ce9612f9c3a033c61093c9ea64b0ec
André <anprofelipe@gmail.com>
2020-08-31 10:06:58 -0300

    Implementando a tela de detalhes do PET

---
+++ lib/Widgets/dots_indicator.dart
@@ -0,0 +1,54 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class DotsIndicator extends AnimatedWidget {
+  DotsIndicator({
+    this.controller,
+    this.itemCount,
+    this.onPageSelected,
+    this.color: Colors.white,
+  }) : super(listenable: controller);
+
+  final PageController controller;
+  final int itemCount;
+  final ValueChanged<int> onPageSelected;
+  final Color color;
+
+  static const double _dotSize = 12.0;
+  static const double _maxZoom = 2.0;
+  static const double _dotSpacing = 50.0;
+
+  Widget _buildDot(int index) {
+    double selected = Curves.easeOut.transform(
+      max(
+        0.0,
+        1.0 - ((controller.page ?? controller.initialPage) - index).abs(),
+      ),
+    );
+    double zoom = 1.0 + (_maxZoom - 1.0) * selected;
+    return Container(
+      width: _dotSpacing,
+      child: Center(
+        child: Material(
+          color: color,
+          type: MaterialType.circle,
+          child: Container(
+            width: _dotSize * zoom,
+            height: _dotSize * zoom,
+            child: InkWell(
+              onTap: () => onPageSelected(index),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List<Widget>.generate(itemCount, _buildDot),
+    );
+  }
+}
