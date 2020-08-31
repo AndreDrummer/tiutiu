28ea0f0068a408d033173bf0a32933475cba7b13
André <anprofelipe@gmail.com>
2020-08-31 10:06:58 -0300

    Implementando a tela de detalhes do PET

--- lib/providers/pets_provider.dart
+++ lib/providers/pets_provider.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:tiutiu/backend/Model/pet_model.dart';
 
