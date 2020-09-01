e503c621903fa33ae29a93e4170d86d9360c6c81
André <anprofelipe@gmail.com>
2020-09-01 20:31:37 -0300

    Alterando tela de detalhes do PET

--- lib/screen/pet_detail.dart
+++ lib/screen/pet_detail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animations/loading_animations.dart';
+import 'package:tiutiu/Widgets/card_details.dart';
 import 'package:tiutiu/Widgets/dots_indicator.dart';
+import 'package:tiutiu/Widgets/favorite_button.dart';
 import 'package:tiutiu/backend/Model/pet_model.dart';
 
 class PetDetails extends StatefulWidget {
@@ -44,214 +46,148 @@ class _PetDetailsState extends State<PetDetails> {
           Column(
             children: [
               Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+                // crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   showImages(pet.photos),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: const EdgeInsets.all(10.0),
-                    color: Colors.blueGrey[50],
-                    child: Column(
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                        Column(
                           children: [
-                            Text(
-                              'Anunciante: ',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
+                            CardDetails(
+                              title: 'Raça',
+                              icon: Icons.adb,
+                              text: pet.breed,
                             ),
-                            Text(
-                              "${pet.ownerName}",
-                            )
+                            CardDetails(
+                              title: 'Tamanho',
+                              icon: Icons.adb,
+                              text: pet.size,
+                            ),
                           ],
                         ),
-                        Row(
+                        Column(
                           children: [
-                            SizedBox(height: 45),
+                            CardDetails(
+                              title: 'Raça',
+                              icon: Icons.adb,
+                              text: pet.breed,
+                            ),
+                            CardDetails(
+                              title: 'Tamanho',
+                              icon: Icons.adb,
+                              text: pet.size,
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        UserCardProfile(
+                          name: pet.ownerName,
+                        ),
+                      ],
+                    ),
+                  ),
+                  FavoriteButton(),
+                  _divider(),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      elevation: 8.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
                             Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Text(
-                                  'Raça: ',
+                                  'Contato:   ',
+                                  textAlign: TextAlign.center,
                                   style: TextStyle(
-                                    fontSize: 18,
                                     fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  "${pet.breed}",
-                                  style: TextStyle(
                                     fontSize: 16,
                                   ),
                                 ),
-                                SizedBox(width: 10),
                                 Text(
-                                  'Tamanho: ',
+                                  '(62) 9 8115-3518',
+                                  textAlign: TextAlign.center,
                                   style: TextStyle(
-                                    fontSize: 18,
                                     fontWeight: FontWeight.bold,
+                                    fontSize: 16,
                                   ),
                                 ),
-                                Text(
-                                  "${pet.size}",
-                                  style: TextStyle(),
+                              ],
+                            ),
+                            SizedBox(height: 16),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.all(20.0),
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.green,
+                                      ),
+                                      child: Icon(
+                                        Icons.phone,
+                                        color: Colors.white,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 12,
+                                    ),
+                                    Text(
+                                      'LIGAR',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.all(20.0),
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.green,
+                                      ),
+                                      child: Icon(
+                                        Icons.phone,
+                                        color: Colors.white,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 12,
+                                    ),
+                                    Text(
+                                      'MENSAGEM',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    )
+                                  ],
                                 )
                               ],
                             ),
                           ],
                         ),
-                        Row(
-                          children: [
-                            Text(
-                              'Idade: ',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              "${pet.ano} anos e  ${pet.meses} meses.",
-                              style: TextStyle(
-                                fontSize: 16,
-                              ),
-                            ),
-                            SizedBox(width: 10),
-                            Text(
-                              'Saúde: ',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              "${pet.health}",
-                              style: TextStyle(
-                                fontSize: 16,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Descrição',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        Divider(),
-                        Container(
-                          width: double.infinity,
-                          padding: const EdgeInsets.all(10.0),
-                          margin: const EdgeInsets.only(top: 5.0),                          
-                          height: 100,
-                          child: SingleChildScrollView(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(pet.details, style: TextStyle(letterSpacing: 1.5)),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  _divider(),
-                  Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Contato:   ',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            '(62) 9 8115-3518',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
                       ),
-                      SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Column(
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(20.0),
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.green,
-                                ),
-                                child: Icon(
-                                  Icons.phone,
-                                  color: Colors.white,
-                                  size: 30,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 12,
-                              ),
-                              Text(
-                                'LIGAR',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              )
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(20.0),
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.green,
-                                ),
-                                child: Icon(
-                                  Icons.phone,
-                                  color: Colors.white,
-                                  size: 30,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 12,
-                              ),
-                              Text(
-                                'MENSAGEM',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ],
-                  )
+                    ),
+                  ),                  
                 ],
               ),
             ],
