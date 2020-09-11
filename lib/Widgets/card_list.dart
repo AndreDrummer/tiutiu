10a9151f0c996ed5e975093d0212112589640f34
André <anprofelipe@gmail.com>
2020-09-11 20:01:35 -0300

    Alterando estrutura da appp, removendo distance matrix

--- lib/Widgets/card_list.dart
+++ lib/Widgets/card_list.dart
@@ -67,167 +67,157 @@ class _CardListState extends State<CardList> {
     UserController user = UserController();
     Authentication auth = Provider.of(context, listen: false);
     FavoritesProvider favoritesProvider = Provider.of(context);
-
-    calculateDistance(widget.petInfo.toMap()['latitude'],
-        widget.petInfo.toMap()['longitude']);
+    
     return InkWell(
       onTap: () {
-        Navigator.pushNamed(context, Routes.PET_DETAILS,
-            arguments: {'petInfo': widget.petInfo, 'kind': widget.kind.toUpperCase()});
+        Navigator.pushNamed(context, Routes.PET_DETAILS, arguments: {
+          'petInfo': widget.petInfo,
+          'kind': widget.kind.toUpperCase()
+        });
       },
       child: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: FutureBuilder(
-          future:
-              // Future.delayed(
-              //   Duration(
-              //     seconds: 1,
-              //   ),
-              // ),
-              calculateDistance(widget.petInfo.toMap()['latitude'],
-                  widget.petInfo.toMap()['longitude']),
-          builder: (_, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: Container(
-                  padding: const EdgeInsets.all(20.0),
-                  child: CircularProgressIndicator(
-                    backgroundColor: Colors.white,
-                  ),
-                ),
-              );
-            }
-            return Card(
-              // color: Colors.white70,
-              elevation: 3,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6.0),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          style: BorderStyle.solid,
-                          color: Colors.blueGrey[50],
-                        ),
+          padding: const EdgeInsets.all(4.0),
+          child: Card(
+            // color: Colors.white70,
+            elevation: 3,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        style: BorderStyle.solid,
+                        color: Colors.blueGrey[50],
                       ),
-                      margin: const EdgeInsets.all(8.0),
-                      child: ClipOval(
-                        child: CircleAvatar(
-                          radius: 50,
-                          backgroundColor: Colors.transparent,
-                          child: FadeInImage(
-                            placeholder: AssetImage('assets/Logo.png'),
-                            image:
-                                NetworkImage(widget.petInfo.toMap()['avatar']),
-                            height: 1000,
-                            width: 1000,
-                            fit: BoxFit.fitWidth,
-                          ),
+                    ),
+                    margin: const EdgeInsets.all(8.0),
+                    child: ClipOval(
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundColor: Colors.transparent,
+                        child: FadeInImage(
+                          placeholder: AssetImage('assets/Logo.png'),
+                          image: NetworkImage(widget.petInfo.toMap()['avatar']),
+                          height: 1000,
+                          width: 1000,
+                          fit: BoxFit.fitWidth,
                         ),
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              widget.petInfo.toMap()['name'],
-                              style: TextStyle(
-                                fontWeight: FontWeight.w900,
-                                fontSize: 16,
-                              ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            widget.petInfo.toMap()['name'],
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 16,
                             ),
-                            widget.favorite
-                                ? IconButton(
-                                    icon: Icon(
-                                        favoritesProvider.getFavoritesPETSIDList.contains(widget.petInfo.toMap()['id'])
-                                            ? Icons.favorite
-                                            : Icons.favorite_border,
-                                        color: Colors.red),
-                                    onPressed: () {
-                                      
-                                      user.favorite(auth.firebaseUser.uid, widget.petInfo.toMap()['petReference'], false);
-                                      favoritesProvider.handleFavorite(widget.petInfo.toMap()['id']);
-                                    },
-                                  )
-                                : SizedBox()
+                          ),
+                          widget.favorite
+                              ? IconButton(
+                                  icon: Icon(
+                                      favoritesProvider.getFavoritesPETSIDList
+                                              .contains(
+                                                  widget.petInfo.toMap()['id'])
+                                          ? Icons.favorite
+                                          : Icons.favorite_border,
+                                      color: Colors.red),
+                                  onPressed: () {
+                                    user.favorite(
+                                        auth.firebaseUser.uid,
+                                        widget.petInfo.toMap()['petReference'],
+                                        false);
+                                    favoritesProvider.handleFavorite(
+                                        widget.petInfo.toMap()['id']);
+                                  },
+                                )
+                              : SizedBox()
+                        ],
+                      ),
+                      SizedBox(height: 8.0),
+                      Text(widget.petInfo.toMap()['breed']),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        // width: MediaQuery.of(context).size.width * 0.525,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            FutureBuilder(
+                                future: loadOwner(
+                                    widget.petInfo.toMap()['ownerReference']),
+                                builder: (context, snapshot) {
+                                  if (snapshot.connectionState ==
+                                      ConnectionState.waiting) {
+                                    return LoadingBumpingLine.circle(size: 20);
+                                  }
+                                  return Text(
+                                    '${snapshot.data} está ${widget.kind.toUpperCase() == 'DONATE' ? 'doando' : 'procurando'}.',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  );
+                                }),
                           ],
                         ),
-                        SizedBox(height: 8.0),
-                        Text(widget.petInfo.toMap()['breed']),
-                        SizedBox(height: 20),
-                        SizedBox(
-                          // width: MediaQuery.of(context).size.width * 0.525,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              FutureBuilder(
-                                  future: loadOwner(
-                                      widget.petInfo.toMap()['ownerReference']),
-                                  builder: (context, snapshot) {
-                                    if (snapshot.connectionState ==
-                                        ConnectionState.waiting) {
-                                      return LoadingBumpingLine.circle(
-                                        backgroundColor: Colors.white,
-                                      );
-                                    }
-                                    return Text(
-                                      '${snapshot.data} está ${widget.kind.toUpperCase() == 'DONATE' ? 'doando' : 'procurando'}.',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    );
-                                  }),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.6,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 7.0),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      'Está a ${snapshot.data[0]}, ',
-                                      // 'Está a 1 km, ',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w700,
-                                      ),
+                      ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            FutureBuilder(
+                                // future: calculateDistance(widget.petInfo.toMap()['latitude'],widget.petInfo.toMap()['longitude']),
+                                future: Future.delayed(Duration(seconds: 1), () {
+                                  return ['100m', '3min'];
+                                }),
+                                builder: (context, snapshot) {
+                                  if (snapshot.connectionState ==
+                                      ConnectionState.waiting) {
+                                    return LoadingBumpingLine.circle(size: 20);
+                                  }
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 7.0),
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          'Está a ${snapshot.data[0]}, ',
+                                          // 'Está a 1 km, ',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                        SizedBox(width: 2),
+                                        Text(
+                                          '${snapshot.data[1]} daqui',
+                                          // '5 min daqui',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                    SizedBox(width: 2),
-                                    Text(
-                                      '${snapshot.data[1]} daqui',
-                                      // '5 min daqui',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
+                                  );
+                                }),
+                          ],
                         ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            );
-          },
-        ),
-      ),
+            ),
+          )),
     );
   }
 }
--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -11,7 +11,7 @@ class FavoritesProvider with ChangeNotifier {
 
   final _favoritesListReference = BehaviorSubject<List<QueryDocumentSnapshot>>();
   final _favoritesPETSList = BehaviorSubject<List<Pet>>();
-  final _favoritesPETSIDList = BehaviorSubject<List<String>>();
+  final _favoritesPETSIDList = BehaviorSubject<List<String>>.seeded([]);
 
   
   Stream<List<QueryDocumentSnapshot>> get favoritesListReference => _favoritesListReference.stream;
@@ -26,7 +26,7 @@ class FavoritesProvider with ChangeNotifier {
   void Function(List<String>) get changeFavoritesPETSIDList => _favoritesPETSIDList.sink.add;
   List<String> get getFavoritesPETSIDList => _favoritesPETSIDList.value;
 
-  void loadFavoritesReference() async {        
+  Future<void> loadFavoritesReference() async {        
     CollectionReference dataBaseCollection = FirebaseFirestore.instance.collection('Users');
     final favoritesListReference = await dataBaseCollection.doc(auth.firebaseUser.uid).collection('Pets').doc('favorites').collection('favorites').get();    
     changefavoritesListReference(favoritesListReference.docs);
