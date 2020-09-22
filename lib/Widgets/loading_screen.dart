43a22838d94cb3eda1260853e185096710101d9c
André <anprofelipe@gmail.com>
2020-09-21 22:21:26 -0300

    Implementando telas de meus pets

---
+++ lib/Widgets/loading_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animations/loading_animations.dart';
+
+class LoadingScreen extends StatelessWidget {
+  
+  LoadingScreen({
+    @required this.text
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          LoadingBumpingLine.circle(
+            backgroundColor: Colors.black,
+          ),
+          SizedBox(height: 15),
+          Text(
+            text,
+            style: Theme.of(context)
+                .textTheme
+                .headline1
+                .copyWith(color: Colors.black),
+          )
+        ],
+      ),
+    );
+  }
+}
