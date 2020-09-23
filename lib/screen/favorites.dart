281706e7e1e381b0cc5f972ad7a8ea90e3682c90
André <anprofelipe@gmail.com>
2020-09-23 08:44:24 -0300

    Melhorando favoritos

--- lib/screen/favorites.dart
+++ lib/screen/favorites.dart
@@ -16,8 +16,7 @@ class _FavoritesState extends State<Favorites> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    favoritesProvider = Provider.of(context, listen: false);
-    favoritesProvider.loadFavoritesReference();
+    favoritesProvider = Provider.of(context, listen: true);
   }
 
   @override
@@ -32,42 +31,47 @@ class _FavoritesState extends State<Favorites> {
       ),
       body: RefreshIndicator(
         onRefresh: () => favoritesProvider.loadFavoritesReference(),
-        child: Stack(
-          children: [
-            Background(),
-            Container(
-              child: StreamBuilder(
-                stream: favoritesProvider.favoritesPETSList,
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return LoadingScreen(text: 'Carregando favoritos');
-                  }
-                  if (snapshot.data.isEmpty) {
-                    return Center(
-                      child: Text(
-                        'Nenhum PET Favoritado',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.headline1.copyWith(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w100,
-                            ),
-                      ),
-                    );
-                  }
-                  return ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (_, index) {
-                      return CardList(
-                        petInfo: snapshot.data[index],
-                        kind: snapshot.data[index].kind,
-                        favorite: true,
+        child: Consumer<FavoritesProvider>(
+          builder: (ctx, favoritesConsumer, _) {
+            return Stack(
+              children: [
+                Background(),
+                Container(
+                  child: StreamBuilder(
+                    stream: favoritesProvider.favoritesPETSList,
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return LoadingScreen(text: 'Carregando favoritos');
+                      }
+                      if (snapshot.data.isEmpty) {
+                        return Center(
+                          child: Text(
+                            'Nenhum PET Favoritado',
+                            textAlign: TextAlign.center,
+                            style:
+                                Theme.of(context).textTheme.headline1.copyWith(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w100,
+                                    ),
+                          ),
+                        );
+                      }
+                      return ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (_, index) {
+                          return CardList(
+                            petInfo: snapshot.data[index],
+                            kind: snapshot.data[index].kind,
+                            favorite: true,
+                          );
+                        },
                       );
                     },
-                  );
-                },
-              ),
-            )
-          ],
+                  ),
+                )
+              ],
+            );
+          },
         ),
       ),
     );
