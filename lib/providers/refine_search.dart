a0f780d902a6bea7de74c896c7d72d459f257a89
André <anprofelipe@gmail.com>
2020-09-30 21:08:52 -0300

    Implementando busca de desaparecidos

--- lib/providers/refine_search.dart
+++ lib/providers/refine_search.dart
@@ -3,53 +3,61 @@ import 'package:rxdart/rxdart.dart';
 
 class RefineSearchProvider with ChangeNotifier {
   final _kindSelected = BehaviorSubject<int>.seeded(0);
-  final _breedsSelected = BehaviorSubject<List<String>>.seeded([]);
-  final _sizesSelected = BehaviorSubject<List<String>>.seeded([]);
-  final _agesSelected = BehaviorSubject<List<String>>.seeded([]);
-  final _healthsSelected = BehaviorSubject<List<String>>.seeded([]);
-  final _distanciesSelected = BehaviorSubject<List<String>>.seeded([]);
+  final _breedSelected = BehaviorSubject<String>.seeded('');
+  final _sizeSelected = BehaviorSubject<String>.seeded('');
+  final _ageSelected = BehaviorSubject<String>.seeded('');
+  final _healthSelected = BehaviorSubject<String>.seeded('');
+  final _distancieSelected = BehaviorSubject<String>.seeded('');
+  final _isDisappeared = BehaviorSubject<bool>.seeded(false);
 
   Stream<int> get kindSelected => _kindSelected.stream;  
-  Stream<List<String>> get breedsSelected => _breedsSelected.stream;  
-  Stream<List<String>> get sizesSelected => _sizesSelected.stream;  
-  Stream<List<String>> get agesSelected => _agesSelected.stream;  
-  Stream<List<String>> get healthsSelected => _healthsSelected.stream;  
-  Stream<List<String>> get distanciesSelected => _distanciesSelected.stream;  
+  Stream<String> get breedSelected => _breedSelected.stream;  
+  Stream<String> get sizeSelected => _sizeSelected.stream;  
+  Stream<String> get ageSelected => _ageSelected.stream;  
+  Stream<String> get healthSelected => _healthSelected.stream;  
+  Stream<String> get distancieSelected => _distancieSelected.stream;  
+  Stream<bool> get isDisappeared => _isDisappeared.stream;  
   
   void changeKindSelected(int kind) {
     _kindSelected.sink.add(kind);
     notifyListeners();
   } 
 
-  void changeBreedsSelected(List<String> newList) {
-    _breedsSelected.sink.add(newList);
+  void changeBreedSelected(String newList) {
+    _breedSelected.sink.add(newList);
     notifyListeners();
   } 
 
-  void changeSizesSelected(List<String> newList) {
-    _sizesSelected.sink.add(newList);
+  void changeSizeSelected(String newList) {
+    _sizeSelected.sink.add(newList);
     notifyListeners();
   } 
 
-  void changeAgesSelected(List<String> newList) {
-    _agesSelected.sink.add(newList);
+  void changeAgeSelected(String newList) {
+    _ageSelected.sink.add(newList);
     notifyListeners();
   }
 
-  void changeHealthsSelected(List<String> newList) {
-    _healthsSelected.sink.add(newList);
+  void changeHealthSelected(String newList) {
+    _healthSelected.sink.add(newList);
     notifyListeners();
   }
 
-  void changeDistanciesSelected(List<String> newList) {
-    _distanciesSelected.sink.add(newList);
+  void changeDistancieSelected(String newList) {
+    _distancieSelected.sink.add(newList);
+    notifyListeners();
+  }  
+
+  void changeIsDisappeared(bool newValue) {
+    _isDisappeared.sink.add(newValue);
     notifyListeners();
   }   
 
   int get getKindSelected => _kindSelected.stream.value; 
-  List<String> get getBreedsSelected => _breedsSelected.stream.value; 
-  List<String> get getSizesSelected => _sizesSelected.stream.value; 
-  List<String> get getAgesSelected => _agesSelected.stream.value; 
-  List<String> get getHealthsSelected => _healthsSelected.stream.value; 
-  List<String> get getDistanciesSelected => _distanciesSelected.stream.value; 
+  String get getBreedSelected => _breedSelected.stream.value; 
+  String get getSizeSelected => _sizeSelected.stream.value; 
+  String get getAgeSelected => _ageSelected.stream.value; 
+  String get getHealthSelected => _healthSelected.stream.value; 
+  String get getDistancieSelected => _distancieSelected.stream.value; 
+  bool get getIsDisappeared => _isDisappeared.stream.value; 
 }
