015be514345d93230caa956249008c1c1a2fa977
André <anprofelipe@gmail.com>
2020-10-12 18:52:59 -0300

    Implementando multi image selector

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,6 +1,6 @@
 class Routes {  
   static const BOOTSTRAP = '/';
-  static const AUTH_HOME = '/auth-home';
+  static const AUTH_HOME = '/';
   static const HOME = '/home';
   static const PET_FORM = '/pet-form';  
   static const MEUS_PETS = '/meus-pets';
