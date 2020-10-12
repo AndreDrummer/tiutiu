c3bda8119f6589fc11ece7418ee4279bcce90cbb
André <anprofelipe@gmail.com>
2020-10-12 18:52:59 -0300

    Implementando multi image selector

--- lib/providers/my_pets_provider.dart
+++ lib/providers/my_pets_provider.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:rxdart/subjects.dart';
 import 'package:tiutiu/backend/Model/pet_model.dart';
 
