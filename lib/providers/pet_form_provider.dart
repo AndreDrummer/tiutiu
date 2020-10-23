6c01cb09c356752d3d0124f2f91638a4457b9032
André <anprofelipe@gmail.com>
2020-10-23 11:53:24 -0300

    Corrigindo erros ao inserir causados pela edição

--- lib/providers/pet_form_provider.dart
+++ lib/providers/pet_form_provider.dart
@@ -83,8 +83,7 @@ class PetFormProvider with ChangeNotifier, FormValidator {
     bool formStatus = true;    
 
     for (BehaviorSubject subject in _subjects()) {
-      if (subject.value == null || subject.value == "") {
-        print("TESTER ${_subjects().indexOf(subject)}");
+      if (subject.value == null || subject.value == "") {        
         subject.addError("*Campo obrigatório");
         formStatus = false;
       }
