9f0687ae30cc21b8f67876a1a05a236c34d7edd3
André <anprofelipe@gmail.com>
2020-10-24 17:50:48 -0300

    Refazendo sistemas de filtragem

--- lib/providers/pets_provider.dart
+++ lib/providers/pets_provider.dart
@@ -1,15 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:tiutiu/backend/Model/pet_model.dart';
 
 class PetsProvider with ChangeNotifier {
   final _petKind = BehaviorSubject<String>();
-  final _petType = BehaviorSubject<String>();  
-  final _breedSelected = BehaviorSubject<String>();  
-  final _sizeSelected = BehaviorSubject<String>();  
-  final _ageSelected = BehaviorSubject<String>();  
-  final _sexSelected = BehaviorSubject<String>();  
-  final _healthSelected = BehaviorSubject<String>();    
+  final _petType = BehaviorSubject<String>();
+  final _breedSelected = BehaviorSubject<String>();
+  final _sizeSelected = BehaviorSubject<String>();
+  final _ageSelected = BehaviorSubject<String>();
+  final _sexSelected = BehaviorSubject<String>();
+  final _healthSelected = BehaviorSubject<String>();
   bool _isFiltering = false;
 
   // Listenning to The Data
@@ -19,7 +20,7 @@ class PetsProvider with ChangeNotifier {
   Stream<String> get sizeSelected => _sizeSelected.stream;
   Stream<String> get ageSelected => _ageSelected.stream;
   Stream<String> get sexSelected => _sexSelected.stream;
-  Stream<String> get healthSelected => _healthSelected.stream;  
+  Stream<String> get healthSelected => _healthSelected.stream;
 
   // Changing the data
   void Function(String) get changePetKind => _petKind.sink.add;
@@ -29,48 +30,69 @@ class PetsProvider with ChangeNotifier {
   void Function(String) get changeAgeSelected => _ageSelected.sink.add;
   void Function(String) get changeSexSelected => _sexSelected.sink.add;
   void Function(String) get changeHealthSelected => _healthSelected.sink.add;
-  
+
   // Getting data
   String get getPetKind => _petKind.value;
-  String get getPetType => _petType.value;  
-  String get getBreedSelected => _breedSelected.value;  
-  String get getSizeSelected => _sizeSelected.value;  
-  String get getAgeSelected => _ageSelected.value;  
-  String get getSexSelected => _sexSelected.value;  
-  String get getHealthSelected => _healthSelected.value;  
-  
+  String get getPetType => _petType.value;
+  String get getBreedSelected => _breedSelected.value;
+  String get getSizeSelected => _sizeSelected.value;
+  String get getAgeSelected => _ageSelected.value;
+  String get getSexSelected => _sexSelected.value;
+  String get getHealthSelected => _healthSelected.value;
+
   void changeIsFiltering(bool status) {
     _isFiltering = status;
   }
 
-  Stream<QuerySnapshot> loadDisappearedPETS() {     
-    return _isFiltering && getPetKind == 'Disappeared' ? loadFilteredPETS() : FirebaseFirestore.instance.collection('Disappeared').snapshots();            
+  List<Pet> getPetListFromSnapshots(List<DocumentSnapshot> docs) {
+    List<Pet> pets = [];    
+    for (int i = 0; i < docs.length; i++) {
+      pets.add(Pet.fromSnapshot(docs[i]));
+    }
+    return pets;
   }
 
-  Stream<QuerySnapshot> loadDonatedPETS() {          
-    return _isFiltering && getPetKind == 'Donate' ? loadFilteredPETS() : FirebaseFirestore.instance.collection('Donate').snapshots();            
+  Stream<QuerySnapshot> loadDisappearedPETS() {
+    return _isFiltering && getPetKind == 'Disappeared'
+        ? loadFilteredPETS()
+        : FirebaseFirestore.instance.collection('Disappeared').snapshots();
+  }
+
+  Stream<QuerySnapshot> loadDonatedPETS() {
+    return _isFiltering && getPetKind == 'Donate'
+        ? loadFilteredPETS()
+        : FirebaseFirestore.instance.collection('Donate').snapshots();
   }
 
   List<String> _filters() {
-    return [getPetType, getBreedSelected, getSizeSelected, getAgeSelected, getSexSelected, getHealthSelected];
+    return [
+      getPetType,
+      getBreedSelected,
+      getSizeSelected,
+      getAgeSelected,
+      getSexSelected,
+      getHealthSelected
+    ];
   }
 
   List<String> _filtersType() {
     return ["type", "breed", "size", "ano", "sex", "health"];
   }
 
-  Stream<QuerySnapshot> loadFilteredPETS() {    
-    Query query = FirebaseFirestore.instance.collection(getPetKind)
-    .where(getPetKind == 'Donate' ? 'donated' : 'found', isEqualTo: false);         
+  Stream<QuerySnapshot> loadFilteredPETS() {
+    Query query = FirebaseFirestore.instance
+        .collection(getPetKind)
+        .where(getPetKind == 'Donate' ? 'donated' : 'found', isEqualTo: false);
 
-    print('Filtro: \n Tipo: $getPetType\nKind: $getPetKind\nRaça: $getBreedSelected\nTamanho: $getSizeSelected\nAno: $getAgeSelected\nSexo: $getSexSelected\nSaúde: $getHealthSelected');
-    
-    for(int i = 0; i < _filters().length; i++) {
-      if(_filters()[i].isNotEmpty && _filters()[i] != null) {        
+    print(
+        'Filtro: \n Tipo: $getPetType\nKind: $getPetKind\nRaça: $getBreedSelected\nTamanho: $getSizeSelected\nAno: $getAgeSelected\nSexo: $getSexSelected\nSaúde: $getHealthSelected');
+
+    for (int i = 0; i < _filters().length; i++) {
+      if (_filters()[i].isNotEmpty && _filters()[i] != null) {
         query = query.where(_filtersType()[i], isEqualTo: _filters()[i]);
       }
-    }  
+    }
 
-    return query.snapshots();            
-  }  
+    return query.snapshots();
+  }
 }
