1354290cec20891d54c11ad6795d82708fc20dc5
André <anprofelipe@gmail.com>
2020-11-05 21:43:40 -0300

    Iniciando implementação da busca na tela de home

--- lib/screen/donate_disappeared_list.dart
+++ lib/screen/donate_disappeared_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tiutiu/Widgets/card_list.dart';
 import 'package:tiutiu/Widgets/error_page.dart';
+import 'package:tiutiu/Widgets/input_search.dart';
 import 'package:tiutiu/Widgets/loading_page.dart';
 import 'package:tiutiu/backend/Model/pet_model.dart';
 import 'package:tiutiu/providers/ads_provider.dart';
@@ -29,12 +30,6 @@ class _DonateDisappearedListState extends State<DonateDisappearedList> {
   AdsProvider adsProvider;
   GlobalKey<ScaffoldState> scaffoldState = GlobalKey();
 
-  void showFilter() {
-    setState(() {
-      filtering = !filtering;
-    });
-  }
-
   void showSnackBar(String content) {
     scaffoldState.currentState.showSnackBar(
       SnackBar(
@@ -42,11 +37,13 @@ class _DonateDisappearedListState extends State<DonateDisappearedList> {
         duration: Duration(milliseconds: 1500),
       ),
     );
-  }  
+  } 
 
   @override
   void didChangeDependencies() {
     adsProvider = Provider.of(context);
+    refineSearchProvider = Provider.of<RefineSearchProvider>(context);
+    petsProvider = Provider.of<PetsProvider>(context);
     super.didChangeDependencies();
   }
 
@@ -62,7 +59,7 @@ class _DonateDisappearedListState extends State<DonateDisappearedList> {
   List<Pet> filterResultsByDistancie(List<Pet> petsListResult) {
     List<Pet> newPetList = [];
     location = Provider.of<Location>(context);
-    refineSearchProvider = Provider.of<RefineSearchProvider>(context);
+    
 
     if (petsListResult != null) {
       for (int i = 0; i < petsListResult.length; i++) {
@@ -91,7 +88,7 @@ class _DonateDisappearedListState extends State<DonateDisappearedList> {
 
   @override
   Widget build(BuildContext context) {
-    final marginTop = MediaQuery.of(context).size.height / 1.2;        
+    final marginTop = MediaQuery.of(context).size.height / 1.55;        
 
     return Scaffold(
       key: _scaffoldKey,
@@ -101,9 +98,12 @@ class _DonateDisappearedListState extends State<DonateDisappearedList> {
         builder: (BuildContext context, AsyncSnapshot snapshot) {
 
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return LoadingPage(
-              messageLoading: 'Carregando PETS perto de você...',
-              circle: true,
+            return Center(
+              child: LoadingPage(
+                messageLoading: 'Carregando PETS perto de você...',                      
+                circle: true,
+                textColor: Colors.black26
+              ),
             );
           }          
 
@@ -132,12 +132,12 @@ class _DonateDisappearedListState extends State<DonateDisappearedList> {
                     MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  SizedBox(height: 10),
+                  // SizedBox(height: 10),
                   // adsProvider.getCanShowAds
                   //     ? adsProvider.bannerAdMob(
                   //         medium_banner: true, adId: adsProvider.topAdId)
                   //     : Container(),
-                  SizedBox(height: 40),
+                  // SizedBox(height: 40),
                   Text(
                     'Nenhum PET encontrado',
                     textAlign: TextAlign.center,
@@ -159,35 +159,45 @@ class _DonateDisappearedListState extends State<DonateDisappearedList> {
           }
 
           return Container(
-            height: marginTop,
-            child: Column(
+            height: marginTop + 30,
+            child: Column(                    
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Column(
-                  children: [
-                    adsProvider.getCanShowAds
-                        ? Container()//adsProvider.bannerAdMob(adId: adsProvider.homeAdId)
-                        : Container(
-                            color: Colors.red,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                'Você está utilizando Tiu, tiu em fase de teste. Os dados podem ser fictícios.',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
+                adsProvider.getCanShowAds
+                    ? Column(
+                      children: [
+                        _HomeSearch(),
+                        Container(                               
+                          alignment: Alignment(-0.9, 1),
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Text('${petsList.length} PETs encontrados', style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black26
+                          )),
+                        ),
+                      ],
+                    )//adsProvider.bannerAdMob(adId: adsProvider.homeAdId)
+                    : Container(
+                        color: Colors.red,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Você está utilizando Tiu, tiu em fase de teste. Os dados podem ser fictícios.',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
                             ),
                           ),
-                  ],
-                ),
+                        ),
+                      ),
                 Expanded(
                   child: ListView.builder(
                     itemCount: petsList.length + 1,
                     itemBuilder: (_, index) {
-                      if (index == petsList.length) {
-                        return SizedBox(height: 50);
+                      if (index == petsList.length && adsProvider.getCanShowAds) {
+                        // return SizedBox(height: 50);
+                        return Container();
                       }
                       return CardList(
                         kind: petsList[index].kind,
@@ -202,5 +212,84 @@ class _DonateDisappearedListState extends State<DonateDisappearedList> {
         },
       ),
     );
+  }
+}
+
+class _HomeSearch extends StatefulWidget {
+  @override
+  __HomeSearchState createState() => __HomeSearchState();
+}
+
+class __HomeSearchState extends State<_HomeSearch> {
+  PetsProvider petsProvider;  
+  RefineSearchProvider refineSearchProvider;
+
+    @override
+  void didChangeDependencies() {    
+    refineSearchProvider = Provider.of<RefineSearchProvider>(context);
+    petsProvider = Provider.of<PetsProvider>(context);
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  StreamBuilder<String>(
+      stream: refineSearchProvider.searchHomeTypeInitialValue,
+      builder: (context, snapshotSearchHomeInitialValue) {
+        return StreamBuilder<List<String>>(
+          stream: refineSearchProvider.searchHomeType,
+          builder: (context, snapshotsnapshotSearchHomeValues) {
+            return StreamBuilder<String>(
+              stream: refineSearchProvider.searchHomePetTypeInitialValue,
+              builder: (context, snapshotHomePetTypeInitialValue) {
+                return StreamBuilder<List<String>>(
+                  stream: refineSearchProvider.searchHomePetType,
+                  builder: (context, snapshotHomePetTypeValues) {
+                    return StreamBuilder<bool>(
+                      stream: refineSearchProvider.searchPetByTypeOnHome,
+                      builder: (context, snapshotSearchPetByTypeOnHome) {
+                        return CustomInput(
+                          searchInitialValue: snapshotSearchHomeInitialValue?.data ?? '',
+                          searchValues: snapshotsnapshotSearchHomeValues?.data ?? [''],
+                          searchPetTypeInitialValue: snapshotHomePetTypeInitialValue?.data ?? '',
+                          searchPetTypeValues: snapshotHomePetTypeValues?.data ?? [''],
+                          isType: snapshotSearchPetByTypeOnHome?.data ?? false,
+                          onDropdownTypeChange: (String text) {
+                            if(text == 'Nome do PET') {
+                              refineSearchProvider.changeSearchPetByTypeOnHome(false);                  
+                              refineSearchProvider.changeSearchHomeTypeInitialValue(text);                   
+                            } else if(text == 'Tipo'){
+                              refineSearchProvider.changeSearchPetByTypeOnHome(true);                                      
+                              refineSearchProvider.changeSearchHomeTypeInitialValue(text);                   
+                            } else {
+                              refineSearchProvider.changeSearchPetByTypeOnHome(false);
+                              refineSearchProvider.changeSearchHomeTypeInitialValue(text);                              
+                            }
+                          },
+                          onDropdownPetTypeChange: (String text) {
+                            refineSearchProvider.changeSearchHomePetTypeInitialValue(text);
+                            petsProvider.changePetType(text);
+                            petsProvider.changeIsFiltering(true);                            
+                            print('Search');
+                          },
+                          onSubmit: (String text){
+                            petsProvider.changeIsFiltering(true);
+                            if(refineSearchProvider.getSearchHomeTypeInitialValue == 'Nome do PET') {
+                              petsProvider.changepetName(text);
+                            } else {
+                              petsProvider.changeBreedSelected(text);
+                            }
+                          }
+                       );
+                     }
+                   );
+                 }
+               );
+             }
+           );
+         }
+       );
+     }
+   );
   }
 }
--- lib/screen/my_pets.dart
+++ lib/screen/my_pets.dart
@@ -49,6 +49,7 @@ class _MyPetsScreenState extends State<MyPetsScreen> {
   }
 
   Stream<QuerySnapshot> myPetsStream() {
+    print("PETS ${widget.kind}");
     if (widget.kind != null) {
       switch (widget.kind) {
         case 'Donate':
