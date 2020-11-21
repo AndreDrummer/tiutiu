9bee5e039902d1845bf033b407ff4bc8b30be817
André <anprofelipe@gmail.com>
2020-11-21 18:58:37 -0300

    Melhorando busca na tela de home

--- lib/Widgets/card_list.dart
+++ lib/Widgets/card_list.dart
@@ -67,8 +67,7 @@ class _CardListState extends State<CardList> {
       children: [
         InkWell(
           onTap: () async {
-            if (userProvider.uid != null &&
-                userProvider.uid != widget.petInfo.ownerId) {
+            if (userProvider.uid != null && userProvider.uid != widget.petInfo.ownerId) {
               PetsProvider().increaseViews(
                 actualViews: widget.petInfo.views ?? 1,
                 petReference: widget.petInfo.petReference,
@@ -128,8 +127,7 @@ class _CardListState extends State<CardList> {
                         bottomLeft: Radius.circular(12),
                         bottomRight: Radius.circular(12),
                       ),
-                      border: Border.all(
-                          style: BorderStyle.solid, color: Colors.grey)),
+                      border: Border.all(style: BorderStyle.solid, color: Colors.grey)),
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Row(
@@ -141,13 +139,7 @@ class _CardListState extends State<CardList> {
                             children: [
                               Text(
                                 widget.petInfo.name,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headline1
-                                    .copyWith(
-                                        fontWeight: FontWeight.w700,
-                                        color: Colors.black,
-                                        fontSize: 25),
+                                style: Theme.of(context).textTheme.headline1.copyWith(fontWeight: FontWeight.w700, color: Colors.black, fontSize: 25),
                               ),
                               SizedBox(height: 10),
                               Text(
@@ -155,73 +147,41 @@ class _CardListState extends State<CardList> {
                               ),
                               SizedBox(height: 10),
                               StreamBuilder(
-                                  stream: UserController().getUserSnapshot(
-                                      widget.petInfo.ownerReference),
+                                  stream: UserController().getUserSnapshot(widget.petInfo.ownerReference),
                                   builder: (context, snapshot) {
-                                    if (snapshot.connectionState ==
-                                            ConnectionState.waiting ||
-                                        snapshot.data == null) {
+                                    if (snapshot.connectionState == ConnectionState.waiting || snapshot.data == null) {
                                       return Text('');
                                     }
 
-                                    String announcerName = snapshot.data
-                                                .data()['uid'] ==
-                                            userProvider.uid
-                                        ? 'Você'
-                                        : snapshot.data.data()['displayName'];
+                                    String announcerName = snapshot.data.data()['uid'] == userProvider.uid ? 'Você' : snapshot.data.data()['displayName'];
                                     return Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       children: [
                                         Text(
                                           '$announcerName está ${widget.kind.toUpperCase() == 'DONATE' ? 'doando' : 'procurando'}.',
                                           textAlign: TextAlign.left,
                                           overflow: TextOverflow.fade,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline1
-                                              .copyWith(
+                                          style: Theme.of(context).textTheme.headline1.copyWith(
                                                 fontWeight: FontWeight.w700,
                                                 color: Colors.black,
                                               ),
                                         ),
                                         Padding(
-                                          padding: const EdgeInsets.only(
-                                              top: 16.0, bottom: 2.0),
+                                          padding: const EdgeInsets.only(top: 16.0, bottom: 2.0),
                                           child: Row(
                                             children: [
-                                              Icon(Tiutiu.eye,
-                                                  size: 14, color: Colors.grey),
-                                              Text(
-                                                  '  ${widget.petInfo.views ?? 1} visualizações',
-                                                  style: TextStyle(
-                                                      color: Colors.grey,
-                                                      fontWeight:
-                                                          FontWeight.w700)),
+                                              Icon(Tiutiu.eye, size: 14, color: Colors.grey),
+                                              Text('  ${widget.petInfo.views ?? 1} visualizações',
+                                                  style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700)),
                                               SizedBox(width: 30),
-                                              Icon(
-                                                  widget.petInfo.kind ==
-                                                          'Donate'
-                                                      ? Icons.favorite
-                                                      : Icons.info,
-                                                  size: 14,
-                                                  color: Colors.grey),
+                                              Icon(widget.petInfo.kind == 'Donate' ? Icons.favorite : Icons.info, size: 14, color: Colors.grey),
                                               StreamBuilder(
-                                                  stream: PetsProvider()
-                                                      .loadInfoOrInterested(
-                                                          kind: widget
-                                                              .petInfo.kind,
-                                                          petReference: widget
-                                                              .petInfo
-                                                              .petReference),
+                                                  stream:
+                                                      PetsProvider().loadInfoOrInterested(kind: widget.petInfo.kind, petReference: widget.petInfo.petReference),
                                                   builder: (context, snapshot) {
                                                     return Text(
                                                         '  ${snapshot.data?.docs?.length ?? 0} ${widget.petInfo.kind == 'Donate' ? 'interessados' : 'informações'}',
-                                                        style: TextStyle(
-                                                            color: Colors.grey,
-                                                            fontWeight:
-                                                                FontWeight
-                                                                    .w700));
+                                                        style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700));
                                                   }),
                                             ],
                                           ),
@@ -238,41 +198,21 @@ class _CardListState extends State<CardList> {
                             IconButton(
                               icon: widget.favorite
                                   ? Icon(
-                                      favoritesProvider.getFavoritesPETSIDList
-                                              .contains(
-                                                  widget.petInfo.toMap()['id'])
-                                          ? Icons.favorite
-                                          : Icons.favorite_border,
+                                      favoritesProvider.getFavoritesPETSIDList.contains(widget.petInfo.toMap()['id']) ? Icons.favorite : Icons.favorite_border,
                                       size: 40,
                                       color: Colors.red,
                                     )
-                                  : Icon(Tiutiu.location_arrow,
-                                      size: 25,
-                                      color: Theme.of(context).primaryColor),
+                                  : Icon(Tiutiu.location_arrow, size: 25, color: Theme.of(context).primaryColor),
                               onPressed: !widget.favorite
                                   ? null
                                   : () {
-                                      if (favoritesProvider
-                                          .getFavoritesPETSIDList
-                                          .contains(
-                                              widget.petInfo.toMap()['id'])) {
-                                        user.favorite(
-                                            userProvider.userReference,
-                                            widget.petInfo
-                                                .toMap()['petReference'],
-                                            false);
-                                        favoritesProvider.handleFavorite(
-                                            widget.petInfo.toMap()['id']);
+                                      if (favoritesProvider.getFavoritesPETSIDList.contains(widget.petInfo.toMap()['id'])) {
+                                        user.favorite(userProvider.userReference, widget.petInfo.toMap()['petReference'], false);
+                                        favoritesProvider.handleFavorite(widget.petInfo.toMap()['id']);
                                       } else {
-                                        user.favorite(
-                                            userProvider.userReference,
-                                            widget.petInfo
-                                                .toMap()['petReference'],
-                                            true);
-                                        favoritesProvider
-                                            .loadFavoritesReference();
-                                        favoritesProvider.handleFavorite(
-                                            widget.petInfo.toMap()['id']);
+                                        user.favorite(userProvider.userReference, widget.petInfo.toMap()['petReference'], true);
+                                        favoritesProvider.loadFavoritesReference();
+                                        favoritesProvider.handleFavorite(widget.petInfo.toMap()['id']);
                                       }
                                     },
                             ),
@@ -281,16 +221,12 @@ class _CardListState extends State<CardList> {
                               mainAxisAlignment: MainAxisAlignment.end,
                               children: <Widget>[
                                 Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 7.0),
+                                  padding: const EdgeInsets.symmetric(vertical: 7.0),
                                   child: Row(
                                     children: [
                                       Text(
                                         '${distanceText[0]}',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headline1
-                                            .copyWith(
+                                        style: Theme.of(context).textTheme.headline1.copyWith(
                                               fontWeight: FontWeight.w700,
                                               color: Colors.black,
                                             ),
