d2c78810852dfe6da297c309c562cc1ea1aacb39
André <anprofelipe@gmail.com>
2020-12-02 15:59:34 -0300

    Finalizando a interface do chat

--- lib/chat/widgets/messages.dart
+++ lib/chat/widgets/messages.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'message_bubble.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
@@ -12,18 +15,26 @@ class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: firestore.collection('Chats').doc(chatId).collection('messages').snapshots(),
+      stream: firestore.collection('Chats').doc(chatId).collection('messages').orderBy('createdAt', descending: true).snapshots(),
       builder: (context, AsyncSnapshot snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
         }
 
-        final chatDocs = snapshot.data.documents;
+        List<DocumentSnapshot> chatDocs = snapshot.data.documents;
         return ListView.builder(
+          reverse: true,
           itemCount: chatDocs.length,
           itemBuilder: (ctx, index) {
-            return Text(
-              chatDocs[index].get('text'),
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2.0),
+              child: MessageBubble(
+                message: chatDocs[index].get('text'),
+                userImage: '', //chatDocs[index].get('userImage'),
+                userName: chatDocs[index].get('userName'),
+                belongToMe: chatDocs[index].get('userId') == 'b', //FirebaseAuth.instance.currentUser.uid,
+                key: ValueKey(chatDocs[index].id),
+              ),
             );
           },
         );
