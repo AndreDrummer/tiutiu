e368de08749dd020004a38c34765821cc75e4283
André <anprofelipe@gmail.com>
2020-12-10 13:30:23 -0300

    Corrigindo alteração das abas desaparecidos e adotar

--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -1,10 +1,12 @@
 class Constantes {
   static const String WEB_API_KEY = 'AIzaSyD0-3gCxQaLljM1fJolnfdpeaUpZsh00XY';
+  static const String DONATE = 'Donate';
+  static const String DISAPPEARED = 'Disappeared';
+  static const String ADOPTED = 'Adopted';
   static const String ANDROID_API_KEY = 'AIzaSyCazQnC5pRqASolYk2wytC4LNNxvw0L-30';
   static const String ADMOB_APP_ID = 'ca-app-pub-2837828701670824~5532122619';
   static const String ADMIN_ID = 'lTndv6cg2BV3uYAnBkLb12XIG083';
   static const String APP_BIRTHDAY = '2020-10-28T18:54:56.905834';
   static const String DYNAMIC_LINK_PREFIX = 'https://tiutiu.page.link';
-  static const String DOG_PLACEHOLDER =
-      'https://firebasestorage.googleapis.com/v0/b/tiutiu-5cd14.appspot.com/o/Nullable%2Fdog%20placeholder.png?alt=media&token=e0cf0960-9748-4087-8824-a26b6909b067';
+  static const String DOG_PLACEHOLDER = 'https://firebasestorage.googleapis.com/v0/b/tiutiu-5cd14.appspot.com/o/Nullable%2Fdog%20placeholder.png?alt=media&token=e0cf0960-9748-4087-8824-a26b6909b067';
 }
