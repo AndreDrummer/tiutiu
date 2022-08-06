cba9b36db203a199f654905efa7f2b9be31e806b
Andre Silva <academicworks.adf@gmail.com>
2022-08-05 21:30:54 -0300

    setting assets file class

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -9,3 +9,15 @@ class RefineSearchStrings {
   static String get all => 'Todos';
   static String get cat => 'Gato';
 }
+
+class LocalPermissionStrings {
+  static String get appBarTitle => 'Permissão para acessar sua localização';
+  static String get petName => 'Nome do Pet';
+  static String get hamster => 'Hamster';
+  static String get dog => 'Cachorro';
+  static String get bird => 'Pássaro';
+  static String get other => 'Outro';
+  static String get type => 'Tipo';
+  static String get all => 'Todos';
+  static String get cat => 'Gato';
+}
