b60bf6ea19d66b36a325142cd36cb87ad936a1fa
Andre Silva <academicworks.adf@gmail.com>
2022-08-05 21:30:54 -0300

    setting assets file class

---
+++ lib/core/constants/text_styles.dart
@@ -0,0 +1,96 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static TextStyle fontSize({
+    FontWeight? fontWeight,
+    Color? color,
+    double? fontSize,
+  }) {
+    return TextStyle(
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize8({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 8.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize10({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 10.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize12({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 12.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize14({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 14.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize16({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 16.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize18({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 18.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize20({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 20.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize22({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 22.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize24({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 24.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+
+  static TextStyle fontSize28({FontWeight? fontWeight, Color? color}) {
+    return TextStyle(
+      fontSize: 28.sp,
+      fontWeight: fontWeight,
+      color: color,
+    );
+  }
+}
