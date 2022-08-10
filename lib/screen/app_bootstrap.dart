d9943e192117ccff594e530d9a39ccc78ce3b1e4
Andre Silva <academicworks.adf@gmail.com>
2022-08-09 21:21:11 -0300

    handling location permission access appropriately

--- lib/screen/app_bootstrap.dart
+++ lib/screen/app_bootstrap.dart
@@ -1,9 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:get/get.dart';
+import 'package:tiutiu/features/location/views/request_current_local_acess_permission_view.dart';
 import 'package:tiutiu/features/location/controller/current_location_controller.dart';
+import 'package:tiutiu/features/location/views/turn_on_localization_service.dart';
+import 'package:tiutiu/Widgets/load_dark_screen.dart';
 import 'package:tiutiu/screen/auth_or_home.dart';
-import 'package:tiutiu/features/location/views/current_local_acess_permission_view.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 final CurrentLocationController _currentLocationController = Get.find();
 
@@ -15,26 +17,56 @@ class AppBootstrap extends StatefulWidget {
 class _BootstrapState extends State<AppBootstrap> {
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => _buildBody(_currentLocationController.locationServiceStatus),
+    return StreamBuilder<ServiceStatus>(
+      stream: Geolocator.getServiceStatusStream(),
+      builder: (context, snapshot) {
+        if (snapshot.data != null) {
+          _currentLocationController.updateLocationServiceStatus();
+        }
+
+        return Obx(
+          () => _currentLocationController.locationServiceStatus ==
+                  LocationServiceStatus.active
+              ? _RequestPermissionsOrHome()
+              : TurnOnLocalizationService(),
+        );
+      },
+    );
+  }
+}
+
+class _RequestPermissionsOrHome extends StatelessWidget {
+  const _RequestPermissionsOrHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _currentLocationController.requestPermission(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting)
+          return Center(
+            child: LoadDarkScreen(),
+          );
+        return Obx(
+          () {
+            final accessDenied = isLocalAccessPermissionDenied(
+              _currentLocationController.permission,
+            );
+
+            if (accessDenied) {
+              return RequestCurrentLocalAccessPermissionView();
+            } else {
+              return AuthOrHome();
+            }
+          },
+        );
+      },
     );
   }
 
-  Widget _buildBody(LocationServiceStatus locationStatus) {
-    if (locationStatus == LocationServiceStatus.deactivated) {
-      return StreamBuilder<ServiceStatus>(
-        stream: Geolocator.getServiceStatusStream(),
-        builder: (context, snapshot) {
-          if (snapshot.data != null) {
-            _currentLocationController.updateLocationServiceStatus();
-          }
-
-          return CurrentLocalAccessPermissionView(
-            locationServiceStatus: locationStatus,
-          );
-        },
-      );
-    }
-    return AuthOrHome();
+  bool isLocalAccessPermissionDenied(
+      LocationPermission currentLocationPermission) {
+    return currentLocationPermission != LocationPermission.always &&
+        currentLocationPermission != LocationPermission.whileInUse;
   }
 }
