14f254576367f1cb3a84b4a8615168da99be14a3
Andre Silva <academicworks.adf@gmail.com>
2022-09-04 10:33:33 -0300

    implementing filter kind in home view

--- lib/Widgets/home_filter_item.dart
+++ lib/Widgets/home_filter_item.dart
@@ -1,46 +1,52 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiutiu/core/constants/text_styles.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class HomeFilterItem extends StatelessWidget {
   const HomeFilterItem({
+    this.isActive = false,
+    required this.kind,
+    required this.icon,
+    this.onItemTap,
     super.key,
-    required this.isActive,
   });
 
+  final Function()? onItemTap;
   final bool isActive;
+  final IconData icon;
+  final String kind;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(width: 1.0.w, color: Colors.green),
-        color: isActive ? Colors.green : Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      margin: EdgeInsets.symmetric(horizontal: 8.0.w, vertical: 4.0.h),
-      height: 56.0.h,
-      width: 56.0.w,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            FontAwesomeIcons.dog,
-            color: isActive ? Colors.white : Colors.green,
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          AutoSizeText(
-            'Cachorro',
-            style: TextStyles.fontSize12(
+    return GestureDetector(
+      onTap: onItemTap,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 1.0.w, color: Colors.green),
+          color: isActive ? Colors.green : Colors.white,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        margin: EdgeInsets.symmetric(horizontal: 2.0.w, vertical: 4.0.h),
+        height: 56.0.h,
+        width: 56.0.w,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
               color: isActive ? Colors.white : Colors.green,
-              fontWeight: FontWeight.w600,
+              icon,
             ),
-          )
-        ],
+            SizedBox(height: 16),
+            AutoSizeText(
+              style: TextStyles.fontSize12(
+                color: isActive ? Colors.white : Colors.green,
+                fontWeight: FontWeight.w600,
+              ),
+              kind,
+            )
+          ],
+        ),
       ),
     );
   }
