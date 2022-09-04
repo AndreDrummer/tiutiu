aba6996b9d9daf95f6c529842143401375c909b9
Andre Silva <academicworks.adf@gmail.com>
2022-09-04 12:12:44 -0300

    filtering by type

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -2,6 +2,7 @@ class AppStrings {
   static String get verifyFilters => 'Verifique seus filtros de busca.';
   static String get wannaLeave => 'Deseja realmente sair?';
   static String get noPetFound => 'Nenhum PET encontrado';
+  static String get backToStart => 'Voltar ao início';
   static String get myFavorites => 'Meus Favoritos';
   static String get endApp => 'Encerrar aplicação';
   static String get loadingDots => 'Carregando...';
@@ -19,16 +20,15 @@ class HomeStrings {
 
 class BottomBarStrings {
   static String get myFavorites => 'Meus Favoritos';
-  static String get myAccount => 'Minha Conta';
 }
 
-class RefineSearchStrings {
+class FilterStrings {
+  static String get disappeared => 'Desaparecido';
   static String get petBreed => 'Raça do Pet';
   static String get petName => 'Nome do Pet';
-  static String get hamster => 'Hamster';
+  static String get exotic => 'Exótico';
   static String get dog => 'Cachorro';
   static String get bird => 'Pássaro';
-  static String get other => 'Outro';
   static String get type => 'Tipo';
   static String get all => 'Todos';
   static String get cat => 'Gato';
