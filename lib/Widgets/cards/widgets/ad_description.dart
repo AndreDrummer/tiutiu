9852117cc0e26fdcebbe13b48ad043877a116352
Andre Silva <academicworks.adf@gmail.com>
2022-09-07 21:28:55 -0300

    isolating card widgets to build a card

---
+++ lib/Widgets/cards/widgets/ad_description.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/core/constants/text_styles.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class AdDescription extends StatelessWidget {
+  const AdDescription({
+    required this.description,
+    super.key,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 8.0.h),
+      child: AutoSizeText(
+        description,
+        style: TextStyles.fontSize14(
+          fontWeight: FontWeight.w400,
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Widgets/cards/widgets/ad_interesteds.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/core/constants/firebase_env_path.dart';
+import 'package:tiutiu/core/constants/strings.dart';
+import 'package:tiutiu/core/constants/text_styles.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class AdInteresteds extends StatelessWidget {
+  const AdInteresteds({
+    required this.petKind,
+    super.key,
+  });
+
+  final String petKind;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 8.0.h),
+      child: Row(
+        children: [
+          Icon(Icons.favorite, size: 12.0.h, color: Colors.grey[400]),
+          Padding(
+            padding: EdgeInsets.only(left: 4.0.w),
+            child: AutoSizeText(
+              '15 ${petKind == FirebaseEnvPath.donate ? '${AppStrings.interesteds}' : '${AppStrings.infos}'}',
+              style: TextStyles.fontSize12(
+                fontWeight: FontWeight.w400,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
