152e8f7f3a0c90f602436a997d45d978c40bf47f
Andre Silva <academicworks.adf@gmail.com>
2022-09-11 18:51:11 -0300

    ordering by distance

--- lib/Widgets/cards/widgets/ad_city_state.dart
+++ lib/Widgets/cards/widgets/ad_city_state.dart
@@ -2,6 +2,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiutiu/core/constants/text_styles.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:tiutiu/core/data/dummy_data.dart';
 
 class AdCityState extends StatelessWidget {
   const AdCityState({
@@ -15,13 +16,18 @@ class AdCityState extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final statesInitials = DummyData.statesInitials;
+    final statesName = DummyData.statesName;
+
+    String UF = statesInitials.elementAt(statesName.indexOf(state));
+
     return Padding(
       padding: EdgeInsets.only(bottom: 8.0.h),
       child: Row(
         children: [
           Icon(Icons.pin_drop, size: 12.0.h, color: Colors.grey[400]),
           AutoSizeText(
-            '$city - $state',
+            '$city - $UF',
             style: TextStyles.fontSize12(
               fontWeight: FontWeight.w700,
               color: Colors.grey,
