c378d09464520e9dc99b1c17f1a18aeb3e9f287b
Andre Silva <academicworks.adf@gmail.com>
2022-09-17 18:40:16 -0300

    implementing scrollable appbar

--- lib/Widgets/cards/widgets/ad_images.dart
+++ lib/Widgets/cards/widgets/ad_images.dart
@@ -22,51 +22,67 @@ class AdImages extends StatelessWidget {
   }
 
   Widget _cardAdImage() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white54,
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(8.0.h),
-          topLeft: Radius.circular(8.0.h),
+    return Hero(
+      tag: photos.first,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white54,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(8.0.h),
+            topLeft: Radius.circular(8.0.h),
+          ),
         ),
-      ),
-      height: Get.height / 1.8,
-      width: double.infinity,
-      child: CarouselSlider.builder(
-        itemCount: photos.length,
-        itemBuilder: (ctx, index, i) {
-          return ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(8),
-              topRight: Radius.circular(8),
-            ),
-            child: Container(
-              width: double.infinity,
-              child: AssetHandle.getImage(photos.elementAt(index)),
-            ),
-          );
-        },
-        options: CarouselOptions(
-          enableInfiniteScroll: photos.length > 1,
-          autoPlayCurve: Curves.easeIn,
-          disableCenter: true,
-          viewportFraction: 1,
-          autoPlay: true,
+        height: Get.height / 1.8,
+        width: double.infinity,
+        child: CarouselSlider.builder(
+          itemCount: photos.length,
+          itemBuilder: (ctx, index, i) {
+            return ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(8),
+                topRight: Radius.circular(8),
+              ),
+              child: Container(
+                width: double.infinity,
+                child: AssetHandle.getImage(photos.elementAt(index)),
+              ),
+            );
+          },
+          options: CarouselOptions(
+            enableInfiniteScroll: photos.length > 1,
+            autoPlayCurve: Curves.easeIn,
+            disableCenter: true,
+            viewportFraction: 1,
+            autoPlay: true,
+          ),
         ),
       ),
     );
   }
 
   Widget _cardAdListImage() {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(12.0.h),
-          topLeft: Radius.circular(12.0.h),
+    return Hero(
+      tag: photos.first,
+      child: Container(
+        child: CarouselSlider.builder(
+          itemCount: photos.length,
+          itemBuilder: (ctx, index, i) {
+            return Container(
+              width: double.infinity,
+              child: AssetHandle.getImage(photos.elementAt(index)),
+            );
+          },
+          options: CarouselOptions(
+            enableInfiniteScroll: photos.length > 1,
+            autoPlayCurve: Curves.easeIn,
+            disableCenter: true,
+            viewportFraction: 1,
+            autoPlay: true,
+          ),
         ),
+        height: Get.height,
+        width: Get.width * .4,
       ),
-      child: AssetHandle.getImage(photos.first),
-      width: Get.width * .4,
     );
   }
 }
