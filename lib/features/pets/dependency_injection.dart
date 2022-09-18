f8368bbd56f45ba053ad31773f90bdfafb37c9e4
Andre Silva <academicworks.adf@gmail.com>
2022-09-18 15:01:11 -0300

    allowing full screen ad images

--- lib/features/pets/dependency_injection.dart
+++ lib/features/pets/dependency_injection.dart
@@ -1,5 +1,6 @@
 import 'package:tiutiu/core/migration/controller/migration_controller.dart';
 import 'package:tiutiu/features/my_pets/controller/my_pets_controller.dart';
+import 'package:tiutiu/features/full_screen/controller/controller.dart';
 import 'package:tiutiu/features/my_pets/services/my_pets_service.dart';
 import 'package:tiutiu/core/migration/service/migration_service.dart';
 import 'package:tiutiu/features/pets/controller/pets_controller.dart';
@@ -13,5 +14,7 @@ class PetControllerDependency extends DependencyInjection {
     Get.lazyPut(() => MigrationController(MigrationService()));
     Get.lazyPut(() => PetsController(PetService.instance));
     Get.lazyPut(() => MyPetsController(MyPetsService()));
+    Get.lazyPut(() => FullscreenController());
+    Get.lazyPut(() => MyPetsService());
   }
 }
