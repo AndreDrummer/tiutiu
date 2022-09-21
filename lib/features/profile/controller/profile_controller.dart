d7dd33033460b4f6a336b200931fd574d9507e9b
Andre Silva <academicworks.adf@gmail.com>
2022-09-20 22:55:16 -0300

    improving user profile

--- lib/features/profile/controller/profile_controller.dart
+++ lib/features/profile/controller/profile_controller.dart
@@ -1,3 +1,10 @@
 import 'package:get/get.dart';
+import 'package:tiutiu/features/system/controllers.dart';
 
-class ProfileController extends GetxController {}
+class ProfileController extends GetxController {
+  Stream<int> getUserPostsCount(String uid) {
+    return tiutiuUserController.service
+        .getUserPostsById(uid)
+        .asyncMap<int>((event) => event.docs.length);
+  }
+}
