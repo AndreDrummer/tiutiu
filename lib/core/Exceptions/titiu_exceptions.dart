94c753afb56f5e0eb91dffcb24725a739873b3da
Andre Silva <academicworks.adf@gmail.com>
2022-09-28 22:02:25 -0300

    working on firebase authentication

--- lib/core/Exceptions/titiu_exceptions.dart
+++ lib/core/Exceptions/titiu_exceptions.dart
@@ -3,22 +3,22 @@ class TiuTiuAuthException {
   final String keyException;
 
   Map<String, String> authErrors = {
-    'ERROR_WEAK_PASSWORD : Password should be at least 6 characters':
+    ErrorCodes.weakPassword:
         'Senha fraca. Escolha uma senha com no mínimo 6 dígitos.',
-    'ERROR_EMAIL_ALREADY_IN_USE':
+    ErrorCodes.emailAlreadyInUse:
         'Esse e-mail já se encontra em nossa base de dados. Tente fazer login.',
-    'ERROR_OPERATION_NOT_ALLOWED': 'Ação não autorizada.',
-    'ERROR_TOO_MANY_REQUESTS':
+    ErrorCodes.operationNotAllowed: 'Ação não autorizada.',
+    ErrorCodes.errorTooManyRequests:
         'Muitas tentativas em curto espaço de tempo. Tente novamente mais tarde.',
-    'too-many-requests':
+    ErrorCodes.tooManyRequests:
         'Muitas tentativas em curto espaço de tempo. Tente novamente mais tarde.',
-    'ERROR_USER_DISABLED': 'Usuário desativado!',
-    'ERROR_WRONG_PASSWORD': 'Senha Incorreta!',
-    'Error validating access token':
+    ErrorCodes.userDisabled: 'Usuário desativado!',
+    ErrorCodes.invalidToken:
         'A sessão foi invalidada porque o usuário mudou sua senha ou o Facebook mudou a sessão por razões de segurança!',
-    'wrong-password': 'Senha Incorreta!',
-    'ERROR_INVALID_EMAIL': 'E-mail Inválido!',
-    'user-not-found': 'E-mail não encontrado!'
+    ErrorCodes.domainUnauthorized: 'Domínio não autorizado!',
+    ErrorCodes.userNotFound: 'E-mail não encontrado!',
+    ErrorCodes.wrongPassword: 'Senha Incorreta!',
+    ErrorCodes.invalidEmail: 'E-mail Inválido!',
   };
 
   @override
@@ -29,4 +29,19 @@ class TiuTiuAuthException {
       return 'Ocorreu um erro inesperado!';
     }
   }
+}
+
+class ErrorCodes {
+  static String get weakPassword =>
+      'ERROR_WEAK_PASSWORD : Password should be at least 6 characters';
+  static String get operationNotAllowed => 'ERROR_OPERATION_NOT_ALLOWED';
+  static String get errorTooManyRequests => 'ERROR_TOO_MANY_REQUESTS';
+  static String get invalidToken => 'Error validating access token';
+  static String get emailAlreadyInUse => 'email-already-in-use';
+  static String get domainUnauthorized => 'unauthorized-domain';
+  static String get tooManyRequests => 'too-many-requests';
+  static String get userDisabled => 'ERROR_USER_DISABLED';
+  static String get invalidEmail => 'ERROR_INVALID_EMAIL';
+  static String get wrongPassword => 'wrong-password';
+  static String get userNotFound => 'user-not-found';
 }
