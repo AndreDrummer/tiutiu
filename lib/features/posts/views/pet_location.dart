5bd59e199f7e5a3716ee6d682a85030e223899a2
Andre Silva <academicworks.adf@gmail.com>
2022-10-08 07:26:36 -0300

    creating steper widget

--- lib/features/posts/views/pet_location.dart
+++ lib/features/posts/views/pet_location.dart
@@ -1,12 +1,7 @@
 import 'package:tiutiu/features/pets/model/pet_model.dart';
-import 'package:tiutiu/core/utils/routes/routes_name.dart';
-import 'package:tiutiu/core/constants/app_colors.dart';
-import 'package:tiutiu/core/constants/strings.dart';
-import 'package:tiutiu/Widgets/button_wide.dart';
-import 'package:tiutiu/Widgets/new_map.dart';
 import 'package:flutter/material.dart';
 
-class PetLocation extends StatefulWidget {
+class PetLocation extends StatelessWidget {
   PetLocation({
     this.editMode = false,
     this.pet,
@@ -14,59 +9,14 @@ class PetLocation extends StatefulWidget {
 
   final bool? editMode;
   final Pet? pet;
-
-  @override
-  _PetLocationState createState() => _PetLocationState();
-}
-
-class _PetLocationState extends State<PetLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.white,
-      // appBar: AppBar(
-      //   title: AutoSizeText(
-      //     kind == FirebaseEnvPath.donate ? 'localização do PET'.toUpperCase() : 'Visto pela última vez em'.toUpperCase(),
-      //     style: Theme.of(context).textTheme.headline4!.copyWith(
-      //           fontSize: 20,
-      //           fontWeight: FontWeight.w700,
-      //         ),
-      //   ),
-      // ),
-      body: Stack(
-        children: <Widget>[
-          NewMap(),
-          Positioned(
-            bottom: 0.0,
-            right: 0.0,
-            left: 0.0,
-            child: StreamBuilder<bool>(
-              // stream: locationProvider.canContinue,
-              builder: (context, snapshot) {
-                return ButtonWide(
-                  color: AppColors.primary,
-                  rounded: false,
-                  isToExpand: true,
-                  text: PostFlowStrings.petIsHere,
-                  action: snapshot.data == null || !snapshot.data!
-                      ? null
-                      : () {
-                          Navigator.pushNamed(
-                            context,
-                            Routes.petForm,
-                          );
-                        },
-                );
-              },
-            ),
-          )
-        ],
+      body: Container(
+        alignment: Alignment.center,
+        color: Colors.black,
+        child: Text('Body'),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 }
