b1d0f8524483be99d7b632052bd2b6ae68ff2f04
Andre Silva <academicworks.adf@gmail.com>
2022-10-07 22:14:40 -0300

    initializing post flow

--- lib/features/home/views/screens.dart
+++ lib/features/home/views/screens.dart
@@ -1,6 +1,7 @@
 import 'package:tiutiu/features/auth/views/authenticated_area.dart';
 import 'package:tiutiu/features/profile/views/edit_profile.dart';
 import 'package:tiutiu/features/profile/views/profile.dart';
+import 'package:tiutiu/features/posts/views/post_flow.dart';
 import 'package:tiutiu/features/pets/views/pets_list.dart';
 import 'package:tiutiu/features/system/controllers.dart';
 import 'package:tiutiu/screen/favorites.dart';
@@ -10,7 +11,7 @@ import 'package:get/get.dart';
 final screens = <Widget>[
   DonateList(),
   DisappearedList(),
-  AuthenticatedArea(child: Favorites()),
+  AuthenticatedArea(child: PostFlow()),
   AuthenticatedArea(
     child: Obx(
       () => IndexedStack(
