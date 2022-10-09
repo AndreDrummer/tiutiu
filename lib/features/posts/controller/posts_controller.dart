ff38ca28e336bc74394f4c2d646c5fb7acbe5de2
Andre Silva <academicworks.adf@gmail.com>
2022-10-09 16:24:06 -0300

    coding stepper

--- lib/features/posts/controller/posts_controller.dart
+++ lib/features/posts/controller/posts_controller.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/widgets.dart';
 import 'package:tiutiu/features/pets/model/pet_model.dart';
 import 'package:get/get.dart';
+import 'package:tiutiu/features/system/controllers.dart';
 
 class PostsController extends GetxController {
+  final GlobalKey<FormState> fullAddressKeyForm = GlobalKey<FormState>();
+  final GlobalKey<FormState> nameKeyForm = GlobalKey<FormState>();
+
   final RxBool _isFullAddress = false.obs;
   final Rx<Pet> _Pet = Pet().obs;
   final RxInt _flowIndex = 0.obs;
@@ -10,15 +15,43 @@ class PostsController extends GetxController {
   int get flowIndex => _flowIndex.value;
   Pet get pet => _Pet.value;
 
-  void set flowIndex(int index) => _flowIndex(index);
-
   void updatePet(PetEnum property, dynamic data) {
     final petMap = pet.toMap();
     petMap[property.name] = data;
 
+    if (pet.owner == null)
+      petMap[PetEnum.owner.name] = tiutiuUserController.tiutiuUser.toMap();
+
     print('>> $petMap');
 
     _Pet(Pet.fromMap(petMap));
+  }
+
+  void onContinue() {
+    switch (flowIndex) {
+      case 0:
+        if (nameKeyForm.currentState!.validate()) {
+          nextStep();
+        }
+        break;
+      case 1:
+        print(isFullAddress);
+        if (!isFullAddress) {
+          nextStep();
+        } else if (isFullAddress &&
+            fullAddressKeyForm.currentState!.validate()) {
+          nextStep();
+        }
+        break;
+    }
+  }
+
+  void nextStep() {
+    _flowIndex(flowIndex + 1);
+  }
+
+  void previousStep() {
+    _flowIndex(flowIndex - 1);
   }
 
   void toggleFullAddress() {
--- lib/features/posts/views/name_and_age.dart
+++ lib/features/posts/views/name_and_age.dart
@@ -1,50 +1,71 @@
 import 'package:tiutiu/Widgets/underline_input_dropdown.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/features/pets/model/pet_model.dart';
+import 'package:tiutiu/features/system/controllers.dart';
+import 'package:tiutiu/core/constants/strings.dart';
 import 'package:tiutiu/Widgets/underline_text.dart';
 import 'package:tiutiu/core/utils/validators.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class NameAndAge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<FormState> _keyForm = GlobalKey<FormState>();
     final monthsList = List.generate(13, (index) => '$index');
     final yearsList = List.generate(22, (index) => '$index');
 
     return Scaffold(
-      body: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0.w),
-            child: Form(
-              key: _keyForm,
-              child: UnderlineInputText(
-                validator: Validators.verifyEmpty,
-                labelText: 'Nome',
+      body: Obx(
+        () => ListView(
+          children: [
+            SizedBox(height: 16.0.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 3.0.w),
+              child: Form(
+                key: postsController.nameKeyForm,
+                child: UnderlineInputText(
+                  initialValue: postsController.pet.name,
+                  validator: Validators.verifyEmpty,
+                  onChanged: (name) {
+                    postsController.updatePet(PetEnum.name, name);
+                  },
+                  labelText: PostFlowStrings.name,
+                  fontSizeLabelText: 16.0.sp,
+                ),
               ),
             ),
-          ),
-          Column(
-            children: [
-              SizedBox(height: 32.0.h),
-              UnderlineInputDropdown(
-                items: yearsList,
-                labelText: 'Anos',
-                initialValue: '0',
-                onChanged: (value) {},
-                fontSize: 22.0.sp,
-              ),
-              SizedBox(height: 32.0.h),
-              UnderlineInputDropdown(
-                items: monthsList,
-                labelText: 'Meses',
-                initialValue: '0',
-                onChanged: (value) {},
-                fontSize: 22.0.sp,
-              ),
-            ],
-          )
-        ],
+            Column(
+              children: [
+                SizedBox(height: 32.0.h),
+                UnderlineInputDropdown(
+                  initialValue: postsController.pet.ageYear.toString(),
+                  onChanged: (anos) {
+                    postsController.updatePet(
+                      PetEnum.ageYear,
+                      int.parse(anos ?? '0'),
+                    );
+                  },
+                  fontSize: 16.0.sp,
+                  labelText: PostFlowStrings.years,
+                  items: yearsList,
+                ),
+                SizedBox(height: 32.0.h),
+                UnderlineInputDropdown(
+                  initialValue: postsController.pet.ageMonth.toString(),
+                  onChanged: (meses) {
+                    postsController.updatePet(
+                      PetEnum.ageMonth,
+                      int.parse(meses ?? '0'),
+                    );
+                  },
+                  labelText: PostFlowStrings.months,
+                  fontSize: 16.0.sp,
+                  items: monthsList,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
