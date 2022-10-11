811a69e78ae4260f283e95738eacb7d2873ed943
Andre Silva <academicworks.adf@gmail.com>
2022-10-10 21:18:18 -0300

    found a solution for dynamic steps

--- lib/features/posts/widgets/stepper.dart
+++ lib/features/posts/widgets/stepper.dart
@@ -18,6 +18,7 @@ class Steper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int n = 7;
     return Card(
       margin: EdgeInsets.all(8.0.h),
       elevation: 8.0.h,
@@ -27,39 +28,51 @@ class Steper extends StatelessWidget {
       child: Container(
         alignment: Alignment.center,
         decoration: BoxDecoration(
-          color: AppColors.black,
           borderRadius: BorderRadius.circular(24.0.h),
+          color: AppColors.black,
         ),
         child: Stack(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: List.generate(
-                stepsName.length,
+                stepsName.length - n,
                 (index) => _SteperItem(
                   isCompleted: currentStep > index,
-                  stepName: stepsName[index],
                   stepsQty: stepsName.length,
+                  stepName: stepsName[index],
                   stepNumber: index + 1,
                   onStepTapped: () {},
                 ),
               ),
             ),
             Positioned(
-              left: 56.0.w,
-              top: 27.0.h,
+              right: 0.0.w,
+              top: 16.0.h,
+              left: 0.w,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: List.generate(
-                  stepsName.length - 1,
+                  stepsName.length - (n + 1),
                   (index) {
-                    return Container(
-                      color: currentStep > index
-                          ? AppColors.primary
-                          : AppColors.white,
-                      margin: EdgeInsets.only(right: 32.0.w),
-                      padding: EdgeInsets.zero,
-                      width: 32.0.w,
-                      height: 1,
+                    return TweenAnimationBuilder(
+                      duration: Duration(milliseconds: 1000),
+                      tween: ColorTween(
+                        begin: currentStep > index
+                            ? AppColors.white
+                            : AppColors.primary,
+                        end: currentStep > index
+                            ? AppColors.primary
+                            : AppColors.white,
+                      ),
+                      builder: (context, color, _) {
+                        return Container(
+                          padding: EdgeInsets.zero,
+                          color: color as Color,
+                          width: 16.0.w,
+                          height: 1,
+                        );
+                      },
                     );
                   },
                 ),
@@ -68,7 +81,7 @@ class Steper extends StatelessWidget {
           ],
         ),
         width: Get.width,
-        height: 72.0.h,
+        height: 56.0.h,
       ),
     );
   }
@@ -94,34 +107,42 @@ class _SteperItem extends StatelessWidget {
     return GestureDetector(
       onTap: onStepTapped,
       child: Container(
-        width: 64.0.w,
+        alignment: Alignment.center,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              margin: EdgeInsets.only(left: 16.0.w, top: 9.0.h),
-              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 8.0.h),
               decoration: BoxDecoration(shape: BoxShape.circle),
-              child: Image.asset(
-                color: isCompleted ? AppColors.primary : AppColors.white,
-                ImageAssets.newLogo,
+              child: TweenAnimationBuilder(
+                duration: Duration(milliseconds: 250),
+                tween: ColorTween(
+                  begin: isCompleted ? AppColors.white : AppColors.primary,
+                  end: isCompleted ? AppColors.primary : AppColors.white,
+                ),
+                builder: (context, color, _) {
+                  return Image.asset(
+                    color: color as Color,
+                    ImageAssets.newLogo,
+                  );
+                },
               ),
-              height: 28.0.h,
-              width: 28.0.h,
+              height: 16.0.h,
+              width: 16.0.h,
             ),
             Container(
-              height: 24.0.h,
-              margin: EdgeInsets.only(top: 4.0.h, left: 16.0.w),
               alignment: Alignment.center,
               child: AutoSizeText(
-                stepName,
-                textAlign: TextAlign.center,
                 style: TextStyles.fontSize(
                   color: isCompleted ? AppColors.primary : AppColors.white,
                   fontWeight: FontWeight.bold,
                 ),
+                textAlign: TextAlign.center,
+                minFontSize: 8.0,
+                maxFontSize: 10.0,
+                stepName,
               ),
+              width: 40.0.w,
+              height: 24.0.h,
             )
           ],
         ),
