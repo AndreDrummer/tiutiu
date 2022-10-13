82fdaf0729f8900546e87f187ce78f05818266d2
Andre Silva <academicworks.adf@gmail.com>
2022-10-12 21:00:36 -0300

    creating pics and videos screen

--- lib/features/posts/views/pictures.dart
+++ lib/features/posts/views/pictures.dart
@@ -1,3 +1,9 @@
+import 'package:get/get.dart';
+import 'package:tiutiu/Widgets/one_line_text.dart';
+import 'package:tiutiu/core/constants/strings.dart';
+import 'package:tiutiu/features/posts/widgets/card_picture.dart';
+import 'package:tiutiu/core/constants/images_assets.dart';
+import 'package:tiutiu/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class Pictures extends StatelessWidget {
@@ -5,8 +11,47 @@ class Pictures extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Fotos'),
+    return Container(
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              itemCount: 1,
+              itemBuilder: (ctx, index) {
+                return AdPicture(
+                  imagePath: ImageAssets.bones,
+                  color: AppColors.primary,
+                  onPicturedRemoved: () {},
+                  onAddPicture: () {},
+                );
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: TextButton.icon(
+              icon: Icon(Icons.add),
+              onPressed: null,
+              label: Text('Adicionar mais fotos'),
+            ),
+          ),
+          Divider(),
+          OneLineText(
+            text: PostFlowStrings.addVideo,
+            alignment: Alignment(-0.92, 1),
+            fontWeight: FontWeight.w500,
+            color: AppColors.secondary,
+          ),
+          Container(
+            height: 200,
+            margin: const EdgeInsets.all(8.0),
+            color: Colors.black,
+          ),
+        ],
+      ),
     );
   }
 }
