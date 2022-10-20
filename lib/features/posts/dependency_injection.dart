91eb8afc39b1bfe3a22f8a572d8bbdccbf1fdb1b
Andre Silva <academicworks.adf@gmail.com>
2022-10-19 22:51:53 -0300

    implementing post upload

--- lib/features/posts/dependency_injection.dart
+++ lib/features/posts/dependency_injection.dart
@@ -1,10 +1,11 @@
 import 'package:tiutiu/features/posts/controller/posts_controller.dart';
 import 'package:tiutiu/core/models/dependency_injection.dart';
 import 'package:get/instance_manager.dart';
+import 'package:tiutiu/features/posts/services/post_service.dart';
 
 class PostsControllerDependency extends DependencyInjection {
   @override
   void init() {
-    Get.lazyPut(() => PostsController());
+    Get.lazyPut(() => PostsController(postService: PostService()));
   }
 }
