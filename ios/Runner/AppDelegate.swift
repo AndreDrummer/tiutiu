ce605137163f6f73cec475445f010edc1f7fc99f
André Felipe <academicworks.adf@gmail.com>
2022-11-02 17:52:47 -0300

    changing pic asset from dialog to bottomsheet

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,12 @@
 import UIKit
 import Flutter
-import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCazQnC5pRqASolYk2wytC4LNNxvw0L-30")
+  ) -> Bool {    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
