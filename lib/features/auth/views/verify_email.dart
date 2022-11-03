e6f62576af38560fcc39c75a76ca9f49c90817cf
André Felipe <academicworks.adf@gmail.com>
2022-11-03 07:22:26 -0300

    validating email and start validating phone number

--- lib/features/auth/views/verify_email.dart
+++ lib/features/auth/views/verify_email.dart
@@ -2,10 +2,19 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiutiu/features/system/controllers.dart';
 import 'package:tiutiu/core/constants/text_styles.dart';
 import 'package:tiutiu/core/constants/app_colors.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class VerifyEmail extends StatelessWidget {
+class VerifyEmail extends StatefulWidget {
   const VerifyEmail({super.key});
+
+  @override
+  State<VerifyEmail> createState() => _VerifyEmailState();
+}
+
+class _VerifyEmailState extends State<VerifyEmail> {
+  bool resendButtonIsEnabled = true;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +22,7 @@ class VerifyEmail extends StatelessWidget {
       children: [
         SizedBox(height: 32.0.h),
         _topBar(),
-        SizedBox(height: 64.0.h),
+        SizedBox(height: 32.0.h),
         _content(),
         Spacer(),
       ],
@@ -28,60 +37,16 @@ class VerifyEmail extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          AutoSizeTexts.autoSizeText18(
-            fontWeight: FontWeight.w600,
-            'Um link de verificação de conta foi enviado',
-            textAlign: TextAlign.center,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AutoSizeTexts.autoSizeText18(
-                fontWeight: FontWeight.w600,
-                textAlign: TextAlign.center,
-                'para o email ',
-              ),
-              AutoSizeTexts.autoSizeText18(
-                fontWeight: FontWeight.w500,
-                textAlign: TextAlign.center,
-                color: AppColors.primary,
-                email,
-              ),
-              AutoSizeTexts.autoSizeText18(
-                fontWeight: FontWeight.w500,
-                textAlign: TextAlign.center,
-                '.',
-              ),
-            ],
-          ),
+          _headline1(),
+          _headline2(email),
           SizedBox(height: 16.0.h),
-          AutoSizeTexts.autoSizeText14(
-            fontWeight: FontWeight.w400,
-            'Somente contas verificadas podem publicar um anúncio. Cheque sua caixa de spam se necessário.',
-            textAlign: TextAlign.center,
-          ),
+          _warningMessage(),
           SizedBox(height: 16.0.h),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              AutoSizeTexts.autoSizeText16(
-                fontWeight: FontWeight.w600,
-                textAlign: TextAlign.center,
-                'Não recebeu um email?',
-              ),
-              SizedBox(width: 4.0.h),
-              AutoSizeTexts.autoSizeText16(
-                textDecoration: TextDecoration.underline,
-                fontWeight: FontWeight.w600,
-                textAlign: TextAlign.center,
-                color: Colors.lightBlue,
-                'Reenviar',
-              ),
-              AutoSizeTexts.autoSizeText16(
-                fontWeight: FontWeight.w600,
-                textAlign: TextAlign.center,
-                '.',
-              ),
+              _noEmailReceived(),
+              _resendButton(),
             ],
           ),
         ],
@@ -101,6 +66,115 @@ class VerifyEmail extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+
+  AutoSizeText _headline1() {
+    return AutoSizeTexts.autoSizeText18(
+      fontWeight: FontWeight.w600,
+      'Um link de verificação foi enviado',
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Row _headline2(String? email) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AutoSizeTexts.autoSizeText18(
+          fontWeight: FontWeight.w600,
+          textAlign: TextAlign.center,
+          'para o email ',
+        ),
+        AutoSizeTexts.autoSizeText18(
+          fontWeight: FontWeight.w500,
+          textAlign: TextAlign.center,
+          color: AppColors.primary,
+          email,
+        ),
+        AutoSizeTexts.autoSizeText18(
+          fontWeight: FontWeight.w500,
+          textAlign: TextAlign.center,
+          '.',
+        ),
+      ],
+    );
+  }
+
+  AutoSizeText _warningMessage() {
+    return AutoSizeTexts.autoSizeText14(
+      fontWeight: FontWeight.w400,
+      'Somente contas verificadas podem publicar um anúncio. Cheque sua caixa de spam se necessário.',
+      textAlign: TextAlign.center,
+    );
+  }
+
+  AutoSizeText _noEmailReceived() {
+    return AutoSizeTexts.autoSizeText16(
+      fontWeight: FontWeight.w600,
+      textAlign: TextAlign.center,
+      'Não recebeu um email?',
+    );
+  }
+
+  Widget _resendButton() {
+    return Visibility(
+      replacement: _loadingWidget(),
+      visible: resendButtonIsEnabled,
+      child: TextButton(
+        child: AutoSizeTexts.autoSizeText16(
+          textDecoration: TextDecoration.underline,
+          fontWeight: FontWeight.w600,
+          textAlign: TextAlign.center,
+          color: Colors.lightBlue,
+          'Reenviar',
+        ),
+        onPressed: _onEmailResent,
+      ),
+    );
+  }
+
+  Future<void> _onEmailResent() async {
+    String resultMessage = '';
+
+    setState(() {
+      resendButtonIsEnabled = false;
+    });
+
+    try {
+      print(authController.user?.email);
+      await authController.user?.sendEmailVerification();
+      resultMessage = 'E-mail enviado!';
+    } catch (exception) {
+      setState(() {
+        resendButtonIsEnabled = true;
+      });
+
+      resultMessage = 'Não foi possível reenviar o e-mail. Tente novamente mais tarde!';
+      debugPrint('>> Could not send the email due to $exception');
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(resultMessage),
+      action: SnackBarAction(
+        label: 'OK',
+        onPressed: () {
+          Get.back();
+        },
+      ),
+    ));
+
+    setState(() {
+      resendButtonIsEnabled = true;
+    });
+  }
+
+  Widget _loadingWidget() {
+    return Container(
+      child: CircularProgressIndicator(strokeWidth: 1),
+      margin: EdgeInsets.all(8.0.h),
+      height: 12.0.h,
+      width: 12.0.h,
     );
   }
 }
