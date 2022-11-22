237c188e248fac8f24df3d7c1924665e6c86e561
André Felipe <academicworks.adf@gmail.com>
2022-11-21 21:07:53 -0300

    implementing delete account logic

--- lib/core/widgets/default_basic_app_bar.dart
+++ lib/core/widgets/default_basic_app_bar.dart
@@ -3,12 +3,16 @@ import 'package:tiutiu/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class DefaultBasicAppBar extends AppBar {
-  DefaultBasicAppBar(
-      {required String text, bool automaticallyImplyLeading = true, List<Widget>? actions, Widget? leading})
-      : super(
+  DefaultBasicAppBar({
+    required String text,
+    bool automaticallyImplyLeading = true,
+    Color? backgroundColor,
+    List<Widget>? actions,
+    Widget? leading,
+  }) : super(
+          backgroundColor: backgroundColor ?? AppColors.primary,
           automaticallyImplyLeading: automaticallyImplyLeading,
           title: AutoSizeTexts.autoSizeText20(text),
-          backgroundColor: AppColors.primary,
           leading: leading,
           actions: actions,
         );
