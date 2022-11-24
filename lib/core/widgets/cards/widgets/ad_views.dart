ff1bd20daca388fd45ca45c65c5f04abf5ed5d74
André Felipe <academicworks.adf@gmail.com>
2022-11-23 22:34:32 -0300

    allowing login with google

--- lib/core/widgets/cards/widgets/ad_views.dart
+++ lib/core/widgets/cards/widgets/ad_views.dart
@@ -1,35 +1,41 @@
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/core/controllers/controllers.dart';
 import 'package:tiutiu/core/constants/text_styles.dart';
+import 'package:tiutiu/features/posts/model/post.dart';
 import 'package:tiutiu/core/constants/strings.dart';
-
 import 'package:flutter/material.dart';
 
 class AdViews extends StatelessWidget {
   const AdViews({
+    required this.post,
     super.key,
-    required this.views,
   });
 
-  final int views;
+  final Post post;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 8.0.h),
-      child: Row(
-        children: [
-          Icon(FontAwesomeIcons.eye, size: 10.0.h, color: Colors.grey[400]),
-          Padding(
-            padding: EdgeInsets.only(left: 4.0.w),
-            child: AutoSizeTexts.autoSizeText12(
-              fontWeight: FontWeight.w400,
-              '$views ${AppStrings.views}',
-              color: Colors.grey,
+    return StreamBuilder<int>(
+        initialData: post.views,
+        stream: postsController.postViews(post.uid!),
+        builder: (context, snapshot) {
+          return Padding(
+            padding: EdgeInsets.only(bottom: 8.0.h),
+            child: Row(
+              children: [
+                Icon(FontAwesomeIcons.eye, size: 10.0.h, color: Colors.grey[400]),
+                Padding(
+                  padding: EdgeInsets.only(left: 4.0.w),
+                  child: AutoSizeTexts.autoSizeText12(
+                    '${snapshot.data ?? post.views} ${AppStrings.views}',
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
+        });
   }
 }
