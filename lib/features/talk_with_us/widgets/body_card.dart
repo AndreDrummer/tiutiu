f5452c34e045b795906c7ee91a20cca4a93c87d3
André Felipe <academicworks.adf@gmail.com>
2022-11-26 15:17:39 -0300

    finishing talk with us functionality

--- lib/features/talk_with_us/widgets/body_card.dart
+++ lib/features/talk_with_us/widgets/body_card.dart
@@ -2,24 +2,31 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 
 class BodyCard extends StatelessWidget {
-  const BodyCard({super.key, required this.child});
+  const BodyCard({
+    required this.bodyHeight,
+    required this.child,
+    super.key,
+  });
 
+  final double bodyHeight;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        bottom: 8.0.h,
-        right: 4.0.w,
-        left: 4.0.w,
-        top: 8.0.h,
+    return AnimatedContainer(
+      duration: Duration(seconds: 1),
+      height: bodyHeight,
+      margin: EdgeInsets.only(
+        bottom: 16.0.h,
+        right: 8.0.w,
+        left: 8.0.w,
+        top: 4.0.h,
       ),
       child: Card(
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0.h),
+          borderRadius: BorderRadius.circular(12.0.h),
         ),
-        elevation: 8.0,
+        elevation: 16.0,
         child: child,
       ),
     );
