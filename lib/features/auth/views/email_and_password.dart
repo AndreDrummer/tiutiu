c0faa5f0c91d9e35246a8befa41e45ae8d91ca03
André Felipe <academicworks.adf@gmail.com>
2022-12-03 09:34:00 -0300

    changing facebook app authentication

--- lib/features/auth/views/email_and_password.dart
+++ lib/features/auth/views/email_and_password.dart
@@ -7,7 +7,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiutiu/core/widgets/load_dark_screen.dart';
 import 'package:tiutiu/core/utils/routes/routes_name.dart';
 import 'package:tiutiu/core/controllers/controllers.dart';
-import 'package:tiutiu/core/widgets/cancel_button.dart';
+import 'package:tiutiu/core/widgets/simple_text_button.dart';
 import 'package:tiutiu/core/mixins/tiu_tiu_pop_up.dart';
 import 'package:tiutiu/core/widgets/one_line_text.dart';
 import 'package:tiutiu/core/constants/app_colors.dart';
