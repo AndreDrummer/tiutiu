41bfd584496591b119a65dd7dc26034a0b632f75
André Felipe <academicworks.adf@gmail.com>
2022-12-04 20:55:47 -0300

    start setting push notifications

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,8 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-'tiutiu/env/{environment}/contacts/{userId}/{contactId}/messages/{messageId}'
-
-
 exports.createNotificationChat = functions.firestore
-    .document('Chats/{chatId}/chat/{messageId}')
+    .document('tiutiu/env/{environment}/contacts/{userId}/{contactId}/messages/{messageId}')
     .onCreate((snap, context) => {
         console.log(snap.data());
         admin.messaging().sendToDevice(`${snap.data()['receiver']['notificationToken']}`, {
