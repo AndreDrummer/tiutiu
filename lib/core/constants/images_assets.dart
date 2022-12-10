763353088d60e6604a8db3fa7217b954a07b2322
André Felipe <academicworks.adf@gmail.com>
2022-12-10 19:24:41 -0300

    adding more some system properties

--- lib/core/constants/images_assets.dart
+++ lib/core/constants/images_assets.dart
@@ -6,20 +6,12 @@ class ImageAssets {
   static String get notFoundET => 'assets/images/not-found-et.webp';
   static String get playstoreLogo => 'assets/images/playstore.webp';
   static String get applestoreLogo => 'assets/images/apple.webp';
-  static String get staticMap => 'assets/images/static_map.webp';
-  static String get sadPanda => 'assets/images/sad-panda.png';
-  static String get patinhas => 'assets/images/patinhas.webp';
   static String get instaLogo => 'assets/images/insta.webp';
-  static String get dogCat2 => 'assets/images/dogCat2.webp';
-  static String get newPin => 'assets/images/new-pin2.webp';
   static String get newLogo => 'assets/images/newLogo.png';
   static String get bones2 => 'assets/images/bones2.webp';
-  static String get dogCat => 'assets/images/dogCat.webp';
   static String get follow => 'assets/images/follow.webp';
   static String get tiutiu => 'assets/images/tiutiu.webp';
-  static String get fadeIn => 'assets/images/fadeIn.webp';
   static String get bones => 'assets/images/bones.webp';
-  static String get fundo => 'assets/images/fundo.webp';
   static String get face => 'assets/images/face.webp';
 }
 
