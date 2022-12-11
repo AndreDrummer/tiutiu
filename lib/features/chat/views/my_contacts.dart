2afc7a7799575907459e814b17f67a5c1b62f4a0
André Felipe <academicworks.adf@gmail.com>
2022-12-11 18:02:16 -0300

    fixing bug on chat

--- lib/features/chat/views/my_contacts.dart
+++ lib/features/chat/views/my_contacts.dart
@@ -1,10 +1,10 @@
-import 'package:tiutiu/core/constants/text_styles.dart';
-import 'package:tiutiu/core/widgets/warning_widget.dart';
 import 'package:tiutiu/features/tiutiu_user/model/tiutiu_user.dart';
 import 'package:tiutiu/features/chat/widgets/contact_tile.dart';
 import 'package:tiutiu/core/widgets/default_basic_app_bar.dart';
-import 'package:tiutiu/features/chat/model/contact.dart';
 import 'package:tiutiu/core/controllers/controllers.dart';
+import 'package:tiutiu/features/chat/model/contact.dart';
+import 'package:tiutiu/core/widgets/warning_widget.dart';
+import 'package:tiutiu/core/constants/text_styles.dart';
 import 'package:tiutiu/core/widgets/async_handler.dart';
 import 'package:tiutiu/core/constants/strings.dart';
 import 'package:flutter/material.dart';
