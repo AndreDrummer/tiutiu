8dcd80bade0b3fcba5d288fdfe40feec6f121fd2
André Felipe <academicworks.adf@gmail.com>
2022-12-10 21:06:33 -0300

    getting device info to feedback action and setting support us screen data controlled via firebase

--- lib/features/support/views/support_us.dart
+++ lib/features/support/views/support_us.dart
@@ -1,12 +1,13 @@
+import 'package:tiutiu/core/extensions/string_extension.dart';
+import 'package:tiutiu/features/support/models/support_us.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiutiu/core/controllers/controllers.dart';
-import 'package:tiutiu/core/constants/images_assets.dart';
 import 'package:tiutiu/core/constants/text_styles.dart';
+import 'package:tiutiu/core/widgets/async_handler.dart';
 import 'package:tiutiu/core/constants/app_colors.dart';
 import 'package:tiutiu/core/widgets/button_wide.dart';
 import 'package:tiutiu/core/utils/asset_handle.dart';
 import 'package:tiutiu/core/constants/strings.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -17,78 +18,75 @@ class SupportUs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final backgroundImages = [
-      DonateAssets.donate1,
-      DonateAssets.donate2,
-      DonateAssets.donate3,
-    ];
-
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppColors.black,
-        body: Stack(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              height: Get.height,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetHandle.imageProvider(backgroundImages.elementAt(Random().nextInt(3))),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Column(
-                children: [
-                  Spacer(),
-                  _copyKey(),
-                  SizedBox(height: 16.0.h),
-                ],
-              ),
-            ),
-            Positioned(
-              child: BackButton(
-                color: AppColors.white,
-              ),
-            )
-          ],
+        body: FutureBuilder<SupportUsData>(
+          future: supportUsController.getSupportUsData(),
+          builder: (context, snapshot) {
+            return AsyncHandler<SupportUsData>(
+              snapshot: snapshot,
+              buildWidget: (supportUsData) {
+                final backgroundImages = supportUsData.backgroundImages;
+
+                return Stack(
+                  children: [
+                    Container(
+                      child: AssetHandle.getImage(backgroundImages.elementAt(Random().nextInt(3))),
+                      decoration: BoxDecoration(color: AppColors.primary),
+                      alignment: Alignment.center,
+                      height: Get.height,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      height: Get.height,
+                      child: Column(
+                        children: [
+                          Spacer(),
+                          _copyKey(context, supportUsData),
+                          SizedBox(height: 16.0.h),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      child: BackButton(
+                        color: AppColors.white,
+                      ),
+                    )
+                  ],
+                );
+              },
+            );
+          },
         ),
       ),
     );
   }
 
-  Widget _copyKey() {
-    final String pixWarning =
-        '<p><b>Aviso:</b> O PIX será destinado a <b>Anja Solutions</b>, empresa que mantém o app ou ao seu sócio <b>André Felipe</b>.</p>';
-
-    return FutureBuilder<String>(
-      future: supportUsController.getPixKey(),
-      builder: (context, snapshot) {
-        final key = snapshot.data ?? '42.510.947/0001-92';
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _keyPix(key),
-            _copyKeyButton(context: context, key: key),
-            _pixWarningMessage(pixWarning),
-          ],
-        );
-      },
-    );
-  }
-
-  Widget _keyPix(String key) {
+  Widget _copyKey(BuildContext context, SupportUsData data) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
+        _keyPix(data.pixKey, data.keyType),
+        _copyKeyButton(context: context, key: data.pixKey),
+        _pixWarningMessage(data.warningMessage),
+      ],
+    );
+  }
+
+  Widget _keyPix(String key, String keyType) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
         Padding(
           child: AutoSizeTexts.autoSizeText10(
-            SupportUsStrings.keyPIX,
             color: AppColors.white,
+            keyType,
           ),
-          padding: EdgeInsets.only(left: 12.0.w, bottom: 8.0.h),
+          padding: EdgeInsets.only(left: 12.0.w, bottom: 2.0.h),
         ),
         AutoSizeTexts.autoSizeText16(
-          key,
+          key.isNotEmptyNeighterNull() ? key : '42.510.947/0001-92',
           textAlign: TextAlign.center,
           fontWeight: FontWeight.w900,
           color: AppColors.white,
@@ -99,6 +97,7 @@ class SupportUs extends StatelessWidget {
 
   ButtonWide _copyKeyButton({required BuildContext context, required String key}) {
     return ButtonWide(
+      color: AppColors.secondary,
       onPressed: () async {
         await Clipboard.setData(ClipboardData(text: key)).then(
           (_) {
@@ -118,17 +117,11 @@ class SupportUs extends StatelessWidget {
 
   Padding _pixWarningMessage(String pixWarning) {
     return Padding(
-      padding: EdgeInsets.only(left: 12.0.w),
-      child: Html(
-        data: pixWarning,
-        style: {
-          'p': Style(
-            fontWeight: FontWeight.w300,
-            fontSize: FontSize.small,
-            margin: EdgeInsets.zero,
-            color: AppColors.white,
-          ),
-        },
+      padding: EdgeInsets.symmetric(horizontal: 12.0.w),
+      child: AutoSizeTexts.autoSizeText12(
+        pixWarning,
+        color: AppColors.white,
+        textAlign: TextAlign.justify,
       ),
     );
   }
