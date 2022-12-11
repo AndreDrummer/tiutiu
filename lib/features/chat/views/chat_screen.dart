23f8f19b948374fb20f5fedcd2e48489debde930
André Felipe <academicworks.adf@gmail.com>
2022-12-11 18:02:16 -0300

    fixing bug on chat

--- lib/features/chat/views/chat_screen.dart
+++ lib/features/chat/views/chat_screen.dart
@@ -11,8 +11,10 @@ import 'package:tiutiu/core/constants/app_colors.dart';
 import 'package:tiutiu/core/utils/asset_handle.dart';
 import 'package:tiutiu/core/constants/strings.dart';
 import 'package:tiutiu/core/utils/dimensions.dart';
+import 'package:tiutiu/core/utils/formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:io';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -24,95 +26,122 @@ class ChatScreen extends StatelessWidget {
         chatController.resetUserChatingWith();
         return true;
       },
-      child: Scaffold(
-        backgroundColor: AppColors.black,
-        appBar: _appBar(),
-        body: Stack(
-          children: [
-            ImageCarouselBackground(),
-            Container(color: AppColors.black.withOpacity(.7)),
-            StreamBuilder<List<Message>>(
-              stream: chatController.messages(loggedUserId),
-              builder: (context, snapshot) {
-                return AsyncHandler<List<Message>>(
-                  emptyWidget: AutoSizeTexts.autoSizeText16(
-                    ChatStrings.startConversation,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.white,
-                  ),
-                  showLoadingScreen: false,
-                  snapshot: snapshot,
-                  buildWidget: (messages) {
-                    return ListView.builder(
-                      itemCount: messages.length,
-                      itemBuilder: ((context, index) {
-                        final previousIndex = index + 1 >= messages.length ? index : index + 1;
-                        final previousMessage = messages[previousIndex];
-                        final message = messages[index];
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: AppColors.black,
+          body: Column(
+            children: [
+              _appBar(),
+              Expanded(
+                child: Stack(
+                  children: [
+                    ImageCarouselBackground(),
+                    Container(color: AppColors.black.withOpacity(.7)),
+                    StreamBuilder<List<Message>>(
+                      stream: chatController.messages(loggedUserId),
+                      builder: (context, snapshot) {
+                        return AsyncHandler<List<Message>>(
+                          emptyWidget: AutoSizeTexts.autoSizeText16(
+                            ChatStrings.startConversation,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.white,
+                          ),
+                          showLoadingScreen: false,
+                          snapshot: snapshot,
+                          buildWidget: (messages) {
+                            return SizedBox(
+                              height: Dimensions.getDimensBasedOnDeviceHeight(
+                                greaterDeviceHeightDouble: Get.height - 208,
+                                minDeviceHeightDouble: Get.height - 144,
+                              ),
+                              child: ListView.builder(
+                                itemCount: messages.length,
+                                itemBuilder: ((context, index) {
+                                  final previousIndex = index + 1 >= messages.length ? index : index + 1;
+                                  final previousMessage = messages[previousIndex];
+                                  final message = messages[index];
 
-                        return Padding(
-                          padding: EdgeInsets.only(
-                            bottom: index > 0
-                                ? 0
-                                : Dimensions.getDimensBasedOnDeviceHeight(
-                                    greaterDeviceHeightDouble: 32.0.h,
-                                    minDeviceHeightDouble: 56.0.h,
-                                  ),
-                          ),
-                          child: MessageBubble(
-                            lastMessageBelongsToTheSameUser: previousMessage.sender.uid == message.sender.uid,
-                            belongToMe: message.sender.uid == loggedUserId,
-                            time: message.createdAt,
-                            message: message.text!,
-                            key: UniqueKey(),
-                          ),
+                                  return Padding(
+                                    padding: EdgeInsets.only(
+                                      bottom: index > 0 ? 0 : 8.0.h,
+                                    ),
+                                    child: MessageBubble(
+                                      lastMessageBelongsToTheSameUser: previousMessage.sender.uid == message.sender.uid,
+                                      belongToMe: message.sender.uid == loggedUserId,
+                                      time: message.createdAt,
+                                      message: message.text!,
+                                      key: UniqueKey(),
+                                    ),
+                                  );
+                                }),
+                                reverse: true,
+                              ),
+                            );
+                          },
                         );
-                      }),
-                      reverse: true,
-                    );
-                  },
-                );
-              },
-            ),
-          ],
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          bottomSheet: NewMessage(),
         ),
-        bottomSheet: NewMessage(),
       ),
     );
   }
 
-  AppBar _appBar() {
-    return AppBar(
-      backgroundColor: AppColors.primary,
-      automaticallyImplyLeading: false,
-      title: Row(
-        children: [
-          BackButton(),
-          GestureDetector(
-            onTap: () => OtherFunctions.navigateToAnnouncerDetail(chatController.userChatingWith, popAndPush: true),
-            child: CircleAvatar(
-              backgroundColor: AppColors.secondary,
-              radius: Dimensions.getDimensBasedOnDeviceHeight(
-                minDeviceHeightDouble: 18.0.h,
-                greaterDeviceHeightDouble: 16.0.h,
-              ),
+  Widget _appBar() {
+    return Container(
+      height: 56.0,
+      width: double.infinity,
+      color: AppColors.primary,
+      child: Container(
+        width: Get.width / 2,
+        child: Row(
+          children: [
+            BackButton(color: AppColors.white),
+            GestureDetector(
+              onTap: () => OtherFunctions.navigateToAnnouncerDetail(chatController.userChatingWith, popAndPush: true),
               child: CircleAvatar(
-                child: ClipOval(
-                  child: AssetHandle.getImage(
-                    chatController.userChatingWith.avatar,
+                backgroundColor: AppColors.secondary,
+                radius: 11.0.h,
+                child: CircleAvatar(
+                  radius: 10.0.h,
+                  child: ClipOval(
+                    child: AssetHandle.getImage(
+                      chatController.userChatingWith.avatar,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Spacer(),
-          AutoSizeTexts.autoSizeText20(
-            OtherFunctions.firstCharacterUpper(
-              chatController.userChatingWith.displayName!.split(' ').first,
+            SizedBox(width: 8.0.w),
+            AutoSizeTexts.autoSizeText16(
+              Formatters.cuttedText(chatController.userChatingWith.displayName!, size: 17),
+              color: AppColors.white,
             ),
-          ),
-          SizedBox(width: Get.width / 2.15),
-        ],
+            Spacer(),
+            PopupMenuButton<String>(
+              icon: Icon(
+                Platform.isIOS ? Icons.more_horiz_outlined : Icons.more_vert_outlined,
+                color: AppColors.white,
+              ),
+              onSelected: (String item) {},
+              itemBuilder: (context) => <PopupMenuEntry<String>>[
+                PopupMenuItem<String>(
+                  value: 'trocarDepois1',
+                  child: Text(ChatStrings.deleteChat),
+                ),
+                PopupMenuItem<String>(
+                  child: Text(ChatStrings.dennounceUser(chatController.userChatingWith.displayName!.split(' ').first)),
+                  value: 'trocarDepois2',
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
