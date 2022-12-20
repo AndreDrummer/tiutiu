f603d73257345d32eb22c17c67f7fb95e5e4110f
André Felipe <academicworks.adf@gmail.com>
2022-12-19 21:44:57 -0300

    updating index.js

--- functions/index.js
+++ functions/index.js
@@ -7,18 +7,23 @@ exports.createNotificationChat = functions.firestore
     .document('tiutiu/env/{environment}/contacts/{userId}/{contactId}/messages/{messageId}')
     .onCreate((snap, context) => {
 
-        console.log(`>> Data ${snap.data()}`);
-        console.log(`>> Resource ${context.resource}`);
-        console.log(`>> Resource ${context.resource}`);
-        
-        admin.messaging().sendToDevice(`${snap.data()['receiver']['notificationToken']}`, {
-            notification: {
-                title: `Nova mensagem de ${snap.data()['sender']['displayName']}`,
-                clickAction: 'FLUTTER_NOTIFICATION_CLICK',
-                body: `${snap.data()['text']}`,
-            },
-            data: {
-                data: JSON.stringify(snap.data())
-            }
-        })
+        console.log(`>> Data ${JSON.stringify(snap.data())}`);
+        console.log(`>> Params ${JSON.stringify(context.params)}`);
+        console.log(`>> Resource ${JSON.stringify(context.resource)}`);
+
+        let senderParamsUid = context.params['userId']
+        let senderDataUid = snap.data()['sender']['uid']
+
+        if(senderDataUid !== senderParamsUid) {
+            admin.messaging().sendToDevice(`${snap.data()['receiver']['notificationToken']}`, {
+                notification: {
+                    title: `Nova mensagem de ${snap.data()['sender']['displayName']}`,
+                    clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+                    body: `${snap.data()['text']}`,
+                },
+                data: {
+                    data: JSON.stringify(snap.data())
+                }
+            })
+        }        
     })
