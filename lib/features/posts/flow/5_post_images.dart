5625744b9390f401a8ea295a3eb7a4ef13a244d0
AndréDrummer <academicworks.adf@gmail.com>
2022-12-27 11:10:56 -0300

    changing gender and age from its places

--- lib/features/posts/flow/5_post_images.dart
+++ lib/features/posts/flow/5_post_images.dart
@@ -1,14 +1,15 @@
 import 'package:tiutiu/core/controllers/controllers.dart';
 import 'package:tiutiu/core/widgets/add_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class PostImages extends StatelessWidget {
   const PostImages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AddImage(
+    return Obx(
+      () => AddImage(
         hasError: postsController.post.photos.isEmpty && !postsController.formIsValid,
         onRemovePictureOnIndex: postsController.removePictureOnIndex,
         onAddPictureOnIndex: postsController.addPictureOnIndex,
