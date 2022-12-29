664935b4dc86612a0e42a37e65e183bcfa5c471f
AndréDrummer <academicworks.adf@gmail.com>
2022-12-29 00:07:11 -0300

    integrating apple with FCM

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Andre Silva on 28/12/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
