e3bad6bec4bf5e064028699fa764f4e3c2598d7d
AndréDrummer <academicworks.adf@gmail.com>
2022-12-30 11:51:17 -0300

    setting up admob

---
+++ lib/features/admob/widgets/ad_banner_300x60.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:tiutiu/core/controllers/controllers.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AdBanner300x60 extends StatefulWidget {
+  const AdBanner300x60({
+    this.borderRadius = BorderRadius.zero,
+    required this.bannerAdId,
+    super.key,
+  });
+
+  final BorderRadiusGeometry? borderRadius;
+  final String bannerAdId;
+
+  @override
+  State<AdBanner300x60> createState() => _AdBanner300x60State();
+}
+
+class _AdBanner300x60State extends State<AdBanner300x60> {
+  @override
+  void initState() {
+    adMobController.updateBannerAdId(widget.bannerAdId);
+    adMobController.bannerAd.load();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ClipRRect(
+        child: AdWidget(ad: adMobController.bannerAd),
+        borderRadius: widget.borderRadius,
+      ),
+      width: Get.width,
+      height: 48.0.h,
+    );
+  }
+
+  @override
+  void dispose() {
+    adMobController.bannerAd.dispose();
+    super.dispose();
+  }
+}
