cd5ae3006de41f953d1826819ae29be78aafbbce
AndréDrummer <academicworks.adf@gmail.com>
2022-12-29 22:57:00 -0300

    fixing issue with the video on small iphone

--- lib/features/posts/views/select_post_type.dart
+++ lib/features/posts/views/select_post_type.dart
@@ -55,7 +55,7 @@ class SelectPostType extends StatelessWidget with TiuTiuPopUp {
             ],
           ),
         ),
-        bottomSheet: _buttons(),
+        bottomNavigationBar: _buttons(),
       ),
     );
   }
