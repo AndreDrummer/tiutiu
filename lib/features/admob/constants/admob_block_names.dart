0bad3255c12ddcb84b18895bc68214e099716252
AndréDrummer <academicworks.adf@gmail.com>
2022-12-30 16:31:09 -0300

    adding rewarded ad on whatsapp click

--- lib/features/admob/constants/admob_block_names.dart
+++ lib/features/admob/constants/admob_block_names.dart
@@ -1,4 +1,10 @@
 class AdMobBlockName {
   static String get postDetailsScreen => 'post_details_screen';
+  static String get whatsaAppNumber => 'whatsapp_number';
   static String get home => 'home';
+}
+
+class AdMobType {
+  static const String rewarded = 'rewarded';
+  static const String banner = 'banner';
 }
--- lib/features/admob/controller/admob_controller.dart
+++ lib/features/admob/controller/admob_controller.dart
@@ -1,5 +1,9 @@
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:get/get.dart';
+import 'package:tiutiu/core/controllers/controllers.dart';
+import 'package:tiutiu/core/local_storage/local_storage.dart';
+import 'package:tiutiu/core/local_storage/local_storage_keys.dart';
+import 'package:tiutiu/features/admob/constants/admob_block_names.dart';
 
 final BannerAdListener _bannerAdlistener = BannerAdListener(
   // Called when an ad is successfully received.
@@ -19,12 +23,17 @@ final BannerAdListener _bannerAdlistener = BannerAdListener(
 );
 
 class AdMobController extends GetxController {
+  int minutesFreeOfRewarded = 2;
+  bool _rewardedAdWasLoaded = false;
+
   final Rx<BannerAd> _bannerAd = BannerAd(
     adUnitId: 'ca-app-pub-3940256099942544/6300978111',
     listener: _bannerAdlistener,
     request: AdRequest(),
     size: AdSize.banner,
   ).obs;
+
+  late RewardedAd _rewardedAd;
 
   BannerAd get bannerAd => _bannerAd.value;
 
@@ -38,6 +47,55 @@ class AdMobController extends GetxController {
         size: AdSize.banner,
         adUnitId: adId,
       ),
+    );
+  }
+
+  Future<void> loadRewardedAd() async {
+    await RewardedAd.load(
+      adUnitId: appController.getAdMobBlockID(blockName: AdMobBlockName.whatsaAppNumber, type: AdMobType.rewarded),
+      request: AdRequest(),
+      rewardedAdLoadCallback: RewardedAdLoadCallback(
+        onAdLoaded: (RewardedAd ad) {
+          print('$ad loaded.');
+          // Keep a reference to the ad so you can show it later.
+          this._rewardedAd = ad;
+          _rewardedAdWasLoaded = true;
+        },
+        onAdFailedToLoad: (LoadAdError error) {
+          print('RewardedAd failed to load: $error');
+        },
+      ),
+    );
+  }
+
+  Future<void> showRewardedAd() async {
+    if (!_rewardedAdWasLoaded) {
+      await loadRewardedAd();
+    }
+
+    _rewardedAd.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (RewardedAd ad) => print('$ad onAdShowedFullScreenContent.'),
+      onAdDismissedFullScreenContent: (RewardedAd ad) {
+        print('$ad onAdDismissedFullScreenContent.');
+        ad.dispose();
+      },
+      onAdFailedToShowFullScreenContent: (RewardedAd ad, AdError error) {
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        ad.dispose();
+      },
+      onAdImpression: (RewardedAd ad) => print('$ad impression occurred.'),
+    );
+
+    _rewardedAd.show(
+      onUserEarnedReward: (AdWithoutView ad, RewardItem rewardItem) async {
+        ad.dispose();
+        print('Usuário assistiu certinho ${ad.adUnitId} ${rewardItem.amount}');
+
+        await LocalStorage.setValueUnderLocalStorageKey(
+          key: LocalStorageKey.lastTimeWatchedARewarded,
+          value: DateTime.now().toIso8601String(),
+        );
+      },
     );
   }
 
