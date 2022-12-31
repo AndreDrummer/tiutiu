bd8a53fdb452f9932fa6a594977641770b7a5065
AndréDrummer <academicworks.adf@gmail.com>
2022-12-31 20:26:19 -0300

    creating dennounce popup

--- lib/features/dennounce/controller/post_dennounce._controller.dart
+++ lib/features/dennounce/controller/post_dennounce._controller.dart
@@ -3,7 +3,7 @@ import 'package:tiutiu/core/constants/strings.dart';
 import 'package:get/get.dart';
 
 class PostDennounceController extends GetxController {
-  final Rx<PostDennounce> _postDennounce = PostDennounce().obs;
+  final Rx<PostDennounce> _postDennounce = PostDennounce(motive: PostDennounceStrings.other).obs;
   final RxInt _postDennounceGroupValue = 3.obs;
   final RxBool _hasError = false.obs;
 
@@ -12,9 +12,20 @@ class PostDennounceController extends GetxController {
   PostDennounce get postDennounce => _postDennounce.value;
   bool get hasError => _hasError.value;
 
-  void set updatePostDennounce(PostDennounce postDennounce) => _postDennounce(postDennounce);
   void set postDennounceGroupValue(int value) => _postDennounceGroupValue(value);
   void set hasError(bool value) => _hasError(value);
+
+  void updatePostDennounce(PostDennounceEnum property, dynamic data) {
+    if (property == PostDennounceEnum.dennouncedPost) _postDennounce(postDennounce.copyWith(dennouncedPost: data));
+    if (property == PostDennounceEnum.description) _postDennounce(postDennounce.copyWith(description: data));
+    if (property == PostDennounceEnum.dennouncer) _postDennounce(postDennounce.copyWith(dennouncer: data));
+    if (property == PostDennounceEnum.motive) _postDennounce(postDennounce.copyWith(motive: data));
+  }
+
+  void resetForm() {
+    _postDennounce(PostDennounce(motive: PostDennounceStrings.other));
+    _postDennounceGroupValue(3);
+  }
 
   final _dennouncePostMotives = [
     PostDennounceStrings.announceNoAnswer,
