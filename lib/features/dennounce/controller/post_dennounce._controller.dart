891f860a5bffcbaed1ccb127c9d31aa86aef2007
AndréDrummer <academicworks.adf@gmail.com>
2023-01-01 17:36:15 -0300

    finishing post dennounce implementation

--- lib/features/dennounce/controller/post_dennounce._controller.dart
+++ lib/features/dennounce/controller/post_dennounce._controller.dart
@@ -1,18 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:tiutiu/core/controllers/controllers.dart';
 import 'package:tiutiu/features/dennounce/model/post_dennounce.dart';
 import 'package:tiutiu/core/constants/strings.dart';
 import 'package:get/get.dart';
+import 'package:tiutiu/features/dennounce/services/dennounce_services.dart';
+import 'package:uuid/uuid.dart';
 
 class PostDennounceController extends GetxController {
-  final Rx<PostDennounce> _postDennounce = PostDennounce(motive: PostDennounceStrings.other).obs;
+  final Rx<PostDennounce> _postDennounce = _defaultPostDennounce().obs;
   final RxInt _postDennounceGroupValue = 3.obs;
+  final RxBool _isLoading = false.obs;
   final RxBool _hasError = false.obs;
 
   int get postDennounceGroupValue => _postDennounceGroupValue.value;
   List<String> get dennouncePostMotives => _dennouncePostMotives;
   PostDennounce get postDennounce => _postDennounce.value;
+  bool get isLoading => _isLoading.value;
   bool get hasError => _hasError.value;
 
   void set postDennounceGroupValue(int value) => _postDennounceGroupValue(value);
+  void set isLoading(bool value) => _isLoading(value);
   void set hasError(bool value) => _hasError(value);
 
   void updatePostDennounce(PostDennounceEnum property, dynamic data) {
@@ -20,11 +27,36 @@ class PostDennounceController extends GetxController {
     if (property == PostDennounceEnum.description) _postDennounce(postDennounce.copyWith(description: data));
     if (property == PostDennounceEnum.dennouncer) _postDennounce(postDennounce.copyWith(dennouncer: data));
     if (property == PostDennounceEnum.motive) _postDennounce(postDennounce.copyWith(motive: data));
+    if (property == PostDennounceEnum.uid) _postDennounce(postDennounce.copyWith(uid: data));
   }
 
   void resetForm() {
-    _postDennounce(PostDennounce(motive: PostDennounceStrings.other));
+    _postDennounce(_defaultPostDennounce());
     _postDennounceGroupValue(3);
+    _hasError(false);
+  }
+
+  void setLoading(bool loadingValue) {
+    _isLoading(loadingValue);
+  }
+
+  Future<void> submit() async {
+    setLoading(true);
+    updatePostDennounce(PostDennounceEnum.dennouncer, tiutiuUserController.tiutiuUser);
+    updatePostDennounce(PostDennounceEnum.uid, Uuid().v4());
+
+    debugPrint('TiuTiuApp: Post Dennounce UID ${postDennounce.uid}');
+
+    DennounceServices().uploadPostDennounceData(postDennounce);
+    resetForm();
+    setLoading(false);
+  }
+
+  static PostDennounce _defaultPostDennounce() {
+    return PostDennounce(
+      dennouncer: tiutiuUserController.tiutiuUser,
+      motive: PostDennounceStrings.other,
+    );
   }
 
   final _dennouncePostMotives = [
