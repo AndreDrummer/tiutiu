ac74fa06e696df9e3562ece21300ac5e5a5769e9
AndréDrummer <academicworks.adf@gmail.com>
2023-01-04 00:06:27 -0300

    adjusting paddings os differentes devices

--- lib/features/admob/widgets/ad_banner_300x60.dart
+++ lib/features/admob/widgets/ad_banner_300x60.dart
@@ -1,9 +1,8 @@
-import 'package:tiutiu/core/constants/app_colors.dart';
-import 'package:tiutiu/core/utils/dimensions.dart';
 import 'package:tiutiu/features/admob/constants/admob_block_names.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:tiutiu/core/controllers/controllers.dart';
+import 'package:tiutiu/core/utils/dimensions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -33,7 +32,7 @@ class _AdBanner300x60State extends State<AdBanner300x60> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: AppColors.black,
+      margin: EdgeInsets.zero,
       child: ClipRRect(
         child: AdWidget(ad: adMobController.bannerAd),
         borderRadius: widget.borderRadius,
