90e194b2e70ad9d14162ac43b23e3e045b2e43e3
AndréDrummer <academicworks.adf@gmail.com>
2023-01-04 00:06:27 -0300

    adjusting paddings os differentes devices

--- lib/core/utils/dimensions.dart
+++ lib/core/utils/dimensions.dart
@@ -1,17 +1,17 @@
 import 'package:get/get.dart';
 
 // Height
-// Nexus 05 --> 616
-// Pixel 06 --> 683
-// Redmi 08 --> 834
-// iPhone 14 -> 932
-// iPhone 07 -> 667
+// iPhone 14 -----> 932 (Big)
+// iPhone 07 -----> 667 (Small)
+// Redmi 08 Pro --> 834 (Medium)
+// Pixel 06 Pro --> 867 (Medium)
+// Nexus 05 ------> 616 (xSmall)
 
 // Aspect Ratio
-// iPhone 07 ------> 0.5622
 // iPhone 14 ------> 0.4613
+// iPhone 07 ------> 0.5622
 // Redmi Note 08 --> 0.4703
-// Pixel 06 -------> 0.4743
+// Pixel 06 Pro----> 0.4743
 // Nexus 05 -------> 0.5844
 
 class Dimensions {
@@ -28,8 +28,13 @@ class Dimensions {
     return (isXSmallDevice() && xSmaller != null) ? xSmaller : smaller;
   }
 
-  static bool isMediumDevice() => Get.height > 690 && Get.height <= 835;
-  static bool isSmallDevice() => Get.height > 650 && Get.height <= 690;
-  static bool isXSmallDevice() => Get.height <= 650;
-  static bool isBigDevice() => Get.height > 835;
+  static bool isSmallDevice() => Get.height > _xSmallDeviceMaxSize && Get.height <= _smallDeviceMaxSize;
+  static bool isMediumDevice() => Get.height > _smallDeviceMaxSize && Get.height <= _mediumDeviceMaxSize;
+  static bool isXSmallDevice() => Get.height <= _xSmallDeviceMaxSize;
+  static bool isBigDevice() => Get.height > _bigDeviceMinimumSize;
+
+  static double _bigDeviceMinimumSize = 900;
+  static double _xSmallDeviceMaxSize = 650;
+  static double _mediumDeviceMaxSize = 900;
+  static double _smallDeviceMaxSize = 750;
 }
