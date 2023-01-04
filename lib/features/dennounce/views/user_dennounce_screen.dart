88f4b85e1977901667e0c9a3c47dc46b8adfca74
AndréDrummer <academicworks.adf@gmail.com>
2023-01-04 00:06:27 -0300

    adjusting paddings os differentes devices

--- lib/features/dennounce/views/user_dennounce_screen.dart
+++ lib/features/dennounce/views/user_dennounce_screen.dart
@@ -19,35 +19,36 @@ class UserDennounceScreen extends StatelessWidget {
       final motiveIsOther = userDennounceController.userDennounce.motive == UserDennounceStrings.other;
       final denounceDescription = userDennounceController.userDennounce.description;
       final motives = userDennounceController.dennounceUserMotives;
+      final hasError = userDennounceController.hasError;
 
       return DennouncePopup(
+        padding: (keyboardIsVisible) => _defaultPadding(
+          keyboardIsVisible: keyboardIsVisible,
+          motiveIsOther: motiveIsOther,
+          hasError: hasError,
+        ),
         groupValue: userDennounceController.userDennounceGroupValue,
         contentHeight: Dimensions.getDimensBasedOnDeviceHeight(
-          smaller: Get.height / (motives.length * 1.3),
-          medium: Get.height / 5.5,
-          bigger: Get.height / 6,
-        ),
-        padding: EdgeInsets.only(
-          bottom: Dimensions.getDimensBasedOnDeviceHeight(
-            smaller: motiveIsOther ? Get.width / 4.5 : Get.width / 3.0,
-            medium: motiveIsOther ? Get.width / 3.2 : Get.width / 2,
-            bigger: Get.width / 2.1,
-          ),
-          top: Dimensions.getDimensBasedOnDeviceHeight(
-            bigger: motiveIsOther ? _topPadding(userDennounceController.hasError) : Get.width / 1.25,
-            smaller: motiveIsOther ? Get.width / 4.25 : Get.width / 2.25,
-            medium: motiveIsOther ? Get.width * .65 : Get.width / 1.2,
-          ),
-          right: Dimensions.getDimensBasedOnDeviceHeight(
-            smaller: 56.0.w,
-            bigger: 56.0.w,
-            medium: 56.0.w,
-          ),
-          left: Dimensions.getDimensBasedOnDeviceHeight(
-            smaller: 56.0.w,
-            bigger: 56.0.w,
-            medium: 56.0.w,
-          ),
+          xSmaller: motiveIsOther
+              ? hasError
+                  ? Get.height / 2.1
+                  : Get.height / 2.2
+              : Get.height / 4.0,
+          smaller: motiveIsOther
+              ? hasError
+                  ? Get.height / 2.2
+                  : Get.height / 2.4
+              : Get.height / 4.0,
+          medium: motiveIsOther
+              ? hasError
+                  ? Get.width / 1.33
+                  : Get.height / 3.1
+              : Get.height / 5.0,
+          bigger: motiveIsOther
+              ? hasError
+                  ? Get.width / 1.45
+                  : Get.height / 3.4
+              : Get.height / 6.0,
         ),
         dennounceMotives: motives,
         onSubmit: () => _onSubmitUserDennounce(context, motiveIsOther),
@@ -107,7 +108,70 @@ class UserDennounceScreen extends StatelessWidget {
     }
   }
 
-  double _topPadding(bool hasError) {
+  EdgeInsetsGeometry _defaultPadding({
+    required bool keyboardIsVisible,
+    required bool motiveIsOther,
+    required bool hasError,
+  }) {
+    return EdgeInsets.only(
+      bottom: Dimensions.getDimensBasedOnDeviceHeight(
+        xSmaller: keyboardIsVisible
+            ? Get.width / 2.25
+            : motiveIsOther
+                ? hasError
+                    ? Get.width / 4.0
+                    : Get.width / 3.6
+                : Get.width / 2.0,
+        smaller: keyboardIsVisible ? Get.width / 1.4 : Get.width / 2.5,
+        medium: keyboardIsVisible
+            ? Get.width / 1.25
+            : motiveIsOther
+                ? hasError
+                    ? Get.width / 3.0
+                    : Get.width / 2.5
+                : Get.width / 2.0,
+        bigger: keyboardIsVisible
+            ? Get.width * .825
+            : motiveIsOther
+                ? Get.width / 3.2
+                : Get.width / 2,
+      ),
+      top: Dimensions.getDimensBasedOnDeviceHeight(
+        xSmaller: keyboardIsVisible
+            ? 0
+            : motiveIsOther
+                ? _topPadding(hasError, keyboardIsVisible)
+                : Get.width / 3.0,
+        bigger: motiveIsOther
+            ? _topPadding(hasError, keyboardIsVisible)
+            : keyboardIsVisible
+                ? Get.width / 1.225
+                : Get.width / 1.3,
+        smaller: motiveIsOther
+            ? _topPadding(hasError, keyboardIsVisible)
+            : keyboardIsVisible
+                ? Get.width / 2.5
+                : Get.width / 2.0,
+        medium: motiveIsOther
+            ? _topPadding(hasError, keyboardIsVisible)
+            : keyboardIsVisible
+                ? Get.width / 1.5
+                : Get.width / 1.3,
+      ),
+      right: Dimensions.getDimensBasedOnDeviceHeight(
+        smaller: 32.0.w,
+        bigger: 56.0.w,
+        medium: 56.0.w,
+      ),
+      left: Dimensions.getDimensBasedOnDeviceHeight(
+        smaller: 32.0.w,
+        bigger: 56.0.w,
+        medium: 56.0.w,
+      ),
+    );
+  }
+
+  double _topPadding(bool hasError, bool keyboardIsVisible) {
     return Dimensions.getDimensBasedOnDeviceHeight(
       smaller: hasError ? Get.width * .65 : Get.width * .225,
       bigger: hasError ? Get.width * .65 : Get.width * .725,
