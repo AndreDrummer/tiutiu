b08c1b95fe51155e54f3190eda3d0d41dfdafd5d
AndréDrummer <academicworks.adf@gmail.com>
2023-01-04 21:50:50 -0300

    writing some migration functions and changing the endpoints sintaxes

--- lib/core/constants/firebase_env_path.dart
+++ lib/core/constants/firebase_env_path.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/foundation.dart';
 
 class FirebaseEnvPath {
+  static String environment = kDebugMode ? EnvironmentBuild.debug.name : EnvironmentBuild.prod.name;
   static const String allowUseWhatsappProdNumber = 'USE_WHATSAPP_PROD_NUMBER';
   static const String whatsappNumberIdDebug = 'WHATSAPP_NUMBER_ID_DEBUG';
   static const String whatsappTemplateDebug = 'WHATSAPP_TEMPLATE_DEBUG';
   static const String whatsappNumberIdProd = 'WHATSAPP_NUMBER_ID_PROD';
   static const String whatsappTemplateProd = 'WHATSAPP_TEMPLATE_PROD';
-  static const String environment = kDebugMode ? 'debug' : 'prod';
   static const String whatsappTokenDebug = 'WHATSAPP_TOKEN_DEBUG';
   static const String whatsappTokenProd = 'WHATSAPP_TOKEN_PROD';
   static const String disappeared = 'Disappeared';
@@ -17,6 +17,11 @@ class FirebaseEnvPath {
   static const String posts = 'posts';
   static const String keys = 'KEYS';
   static const String env = 'env';
+}
+
+enum EnvironmentBuild {
+  debug,
+  prod,
 }
 
 enum FileType {
