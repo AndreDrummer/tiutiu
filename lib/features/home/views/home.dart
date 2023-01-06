9f97b81699f275b9cff982f85130441c9621d3f7
AndréDrummer <academicworks.adf@gmail.com>
2023-01-06 20:18:37 -0300

    updating ad shared text

--- lib/features/home/views/home.dart
+++ lib/features/home/views/home.dart
@@ -49,6 +49,7 @@ class _HomeState extends State<Home> with TiuTiuPopUp {
               backGroundColor: AppColors.warning,
               message: AppStrings.wannaLeave,
               confirmText: AppStrings.yes,
+              textColor: AppColors.black,
               barrierDismissible: false,
               title: AppStrings.endApp,
               denyText: AppStrings.no,
