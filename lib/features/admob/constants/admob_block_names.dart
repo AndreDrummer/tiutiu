e733f1a2a4d19c08a9449a3e85a38ea5bec15c54
AndréDrummer <academicworks.adf@gmail.com>
2023-01-05 22:00:34 -0300

    creating AdMob block ID differents based on platform

--- lib/features/admob/constants/admob_block_names.dart
+++ lib/features/admob/constants/admob_block_names.dart
@@ -1,12 +1,39 @@
-class AdMobBlockName {
-  static String get DennouncePopup => 'post_dennounce_screen';
-  static String get postDetailsScreen => 'post_details_screen';
-  static String get whatsaAppNumber => 'whatsapp_number';
-  static String get home => 'home';
-}
+import 'dart:io';
 
 class AdMobType {
   static const String interstitial = 'interstitial';
   static const String rewarded = 'rewarded';
   static const String banner = 'banner';
+}
+
+class AdMobBlockName {
+  static final isOS = Platform.isIOS;
+
+  static final String whatsappQuoteAdBlockId =
+      isOS ? _AdMobBlockNames.on_whatsapp_quote_expires_ios.name : _AdMobBlockNames.on_whatsapp_quote_expires.name;
+
+  static final String chatQuoteAdBlockId =
+      isOS ? _AdMobBlockNames.on_chat_quote_expires_ios.name : _AdMobBlockNames.on_chat_quote_expires.name;
+
+  static final String homeFooterAdBlockId =
+      isOS ? _AdMobBlockNames.home_footer_ios.name : _AdMobBlockNames.home_footer.name;
+
+  static final String openingAdBlockId = isOS ? _AdMobBlockNames.opening_ios.name : _AdMobBlockNames.opening.name;
+
+  static final String postDetailScreenAdBlockId =
+      isOS ? _AdMobBlockNames.post_detail_screen_ios.name : _AdMobBlockNames.post_detail_screen.name;
+}
+
+enum _AdMobBlockNames {
+  on_whatsapp_quote_expires,
+  on_chat_quote_expires,
+  post_detail_screen,
+  home_footer,
+  opening,
+
+  on_whatsapp_quote_expires_ios,
+  on_chat_quote_expires_ios,
+  post_detail_screen_ios,
+  home_footer_ios,
+  opening_ios,
 }
