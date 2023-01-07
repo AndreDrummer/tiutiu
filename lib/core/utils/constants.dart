e43e8ef46f866ed19dc76b150d32e75dc72d1ef6
AndréDrummer <academicworks.adf@gmail.com>
2023-01-07 18:15:02 -0300

    setting social link to be settable remotly

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,6 +1,4 @@
 class Constants {
-  static const String APP_FACEBOOK_PAGE = 'https://www.facebook.com/profile.php?id=100087589894761';
-  static const String APP_INSTAGRAM_PAGE = 'https://www.instagram.com/tiutiuapp/';
   static const String APP_IOS_BUNDLE_ID = 'com.anjasolutions.tiutiuapp';
   static const String DYNAMIC_LINK_PREFIX = 'https://tiutiu.page.link';
   static const String APP_ANDROID_ID = 'com.anjasolutions.tiutiu';
