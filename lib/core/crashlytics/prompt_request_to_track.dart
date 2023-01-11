cf604ab452d20a0ed7304e33c912c23f0e2e8d0b
AndréDrummer <academicworks.adf@gmail.com>
2023-01-10 22:44:19 -0300

    setting up firebase crashlytics on project: not automatically sending reports

---
+++ lib/core/crashlytics/prompt_request_to_track.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PrompRequestToTrack extends StatelessWidget {
+  const PrompRequestToTrack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
