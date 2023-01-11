fa6406fae37ffdef964f2d6af02839626c92a26b
AndréDrummer <academicworks.adf@gmail.com>
2023-01-10 22:25:44 -0300

    adding firebase crashlytics: working on iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -125,6 +124,8 @@ Future<void> main() async {
   FirebaseMessaging.onMessage.listen(_firebaseMessagingForegroundHandler);
 
   MobileAds.instance.initialize();
+
+  crashlyticsController.init();
 
   initializeDateFormatting();
 
