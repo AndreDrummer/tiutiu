faaa0f3c0e57d231ee4c1fbb9eec85bfa46baf30
AndréDrummer <academicworks.adf@gmail.com>
2023-01-10 21:46:22 -0300

    adding crashlytics: working on android

---
+++ lib/core/crashlytics/crashlytics_controller.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
+
+class CrashlyticsController extends GetxController {
+  Future<void> init() async {
+    FirebaseCrashlytics.instance.crash();
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(!kDebugMode);
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  }
+}
