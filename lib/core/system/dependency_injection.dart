de286da1ad4c89017e0c56d50a2ac28aaffef8e3
AndréDrummer <academicworks.adf@gmail.com>
2023-01-10 21:46:22 -0300

    adding crashlytics: working on android

--- lib/core/system/dependency_injection.dart
+++ lib/core/system/dependency_injection.dart
@@ -1,4 +1,5 @@
 import 'package:tiutiu/core/system/controller/system_controller.dart';
+import 'package:tiutiu/core/crashlytics/crashlytics_controller.dart';
 import 'package:tiutiu/core/full_screen/controller/controller.dart';
 import 'package:tiutiu/core/system/service/system_service.dart';
 import 'package:tiutiu/core/models/dependency_injection.dart';
@@ -7,6 +8,7 @@ import 'package:get/instance_manager.dart';
 class SystemControllerDependency extends DependencyInjection {
   @override
   void init() {
+    Get.lazyPut(() => CrashlyticsController());
     Get.lazyPut(() => SystemService());
     Get.lazyPut(() => SystemController(systemService: Get.find()));
     Get.lazyPut(() => FullscreenController());
