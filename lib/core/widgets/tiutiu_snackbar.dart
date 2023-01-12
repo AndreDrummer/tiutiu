f507435a02bbd4a2dc1c051047aba3b1984ef417
AndréDrummer <academicworks.adf@gmail.com>
2023-01-12 11:03:13 -0300

    fixing video player on post details

--- lib/core/widgets/tiutiu_snackbar.dart
+++ lib/core/widgets/tiutiu_snackbar.dart
@@ -1,7 +1,6 @@
 import 'package:tiutiu/core/constants/text_styles.dart';
 import 'package:tiutiu/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class TiuTiuSnackBar extends SnackBar {
   TiuTiuSnackBar({super.key, required this.message, this.color})
@@ -9,13 +8,6 @@ class TiuTiuSnackBar extends SnackBar {
           backgroundColor: color ?? AppColors.info,
           content: AutoSizeTexts.autoSizeText14(message),
           duration: Duration(milliseconds: 1500),
-          action: SnackBarAction(
-            textColor: AppColors.white,
-            label: 'OK',
-            onPressed: () {
-              Get.back();
-            },
-          ),
         );
 
   final Color? color;
