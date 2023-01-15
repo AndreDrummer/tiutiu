37477b7307faac02f25412cc4c0967463b877742
AndréDrummer <academicworks.adf@gmail.com>
2023-01-15 16:27:25 -0300

    setting videos scroll to work tik tok alike

---
+++ lib/core/widgets/loading_video.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/core/widgets/lottie_animation.dart';
+import 'package:tiutiu/core/constants/text_styles.dart';
+import 'package:tiutiu/core/constants/assets_path.dart';
+import 'package:tiutiu/core/constants/app_colors.dart';
+import 'package:tiutiu/core/constants/strings.dart';
+import 'package:flutter/material.dart';
+
+class LoadingVideo extends StatelessWidget {
+  const LoadingVideo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        LottieAnimation(animationPath: AnimationsAssets.pawLoading2, size: 64),
+        SizedBox(height: 8.0.h),
+        AutoSizeTexts.autoSizeText14(
+          AppStrings.loadingVideo,
+          color: AppColors.white,
+        ),
+      ],
+    );
+  }
+}
