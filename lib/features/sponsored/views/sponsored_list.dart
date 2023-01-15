66b56ad3a97313a168c94a770b7dff683da8435b
AndréDrummer <academicworks.adf@gmail.com>
2023-01-15 00:50:40 -0300

    finishing sponsored ads implementation

---
+++ lib/features/sponsored/views/sponsored_list.dart
@@ -0,0 +1,67 @@
+import 'package:tiutiu/features/sponsored/model/sponsored.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/core/utils/launcher_functions.dart';
+import 'package:tiutiu/core/controllers/controllers.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:tiutiu/core/utils/asset_handle.dart';
+import 'package:tiutiu/core/utils/dimensions.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SponsoredList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double sponsoredAdsTileSize = Dimensions.getDimensBasedOnDeviceHeight(
+      bigger: 68.0.h,
+      medium: 68.0.h,
+      smaller: 80.0.h,
+    );
+
+    return StreamBuilder<List<Sponsored>>(
+      stream: sponsoredController.sponsoredAds(),
+      builder: (context, snapshot) {
+        final sponsoreds = snapshot.data ?? [];
+
+        final visible = adminRemoteConfigController.configs.showSponsoredAds &&
+            systemController.properties.internetConnected &&
+            sponsoreds.isNotEmpty;
+
+        return Visibility(
+          visible: visible,
+          child: CarouselSlider.builder(
+            itemCount: sponsoreds.length,
+            itemBuilder: (context, index, realIndex) {
+              final sponsored = sponsoreds[index];
+              return GestureDetector(
+                onTap: () {
+                  Launcher.openBrowser(sponsored.link!);
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0.h)),
+                  child: Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(12.0.h)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12.0.h),
+                      child: AssetHandle.getImage(sponsored.imagePath),
+                    ),
+                    alignment: Alignment.center,
+                    width: Get.width,
+                  ),
+                  margin: EdgeInsets.only(top: 4.0.h),
+                ),
+              );
+            },
+            options: CarouselOptions(
+              enableInfiniteScroll: sponsoreds.length > 1,
+              autoPlayCurve: Curves.easeIn,
+              height: sponsoredAdsTileSize,
+              enlargeCenterPage: true,
+              viewportFraction: 1,
+              autoPlay: true,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
