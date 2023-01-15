c46efbf86dcd0a67622d1ae1e64060849a8729ff
AndréDrummer <academicworks.adf@gmail.com>
2023-01-15 01:47:11 -0300

    implementing the like in big card

--- lib/core/widgets/cards/widgets/ad_city_state.dart
+++ lib/core/widgets/cards/widgets/ad_city_state.dart
@@ -1,8 +1,9 @@
 import 'package:tiutiu/features/home/controller/home_controller.dart';
+import 'package:tiutiu/core/location/models/states_and_cities.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiutiu/core/controllers/controllers.dart';
-import 'package:tiutiu/core/location/models/states_and_cities.dart';
 import 'package:tiutiu/core/constants/text_styles.dart';
+import 'package:tiutiu/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AdCityState extends StatelessWidget {
@@ -17,20 +18,21 @@ class AdCityState extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final rightSide = homeController.cardVisibilityKind == CardVisibilityKind.card;
+    final isCardVisibility = homeController.cardVisibilityKind == CardVisibilityKind.card;
+
     final statesInitials = StatesAndCities.stateAndCities.stateInitials;
     final statesName = StatesAndCities.stateAndCities.stateNames;
 
     String UF = statesInitials.elementAt(statesName.indexOf(state));
 
     return Container(
-      margin: EdgeInsets.only(bottom: rightSide ? 8.0.h : 0.0),
-      alignment: rightSide ? Alignment.centerRight : null,
-      width: rightSide ? 200.0.w : null,
+      margin: EdgeInsets.only(bottom: isCardVisibility ? 8.0.h : 0.0),
+      alignment: isCardVisibility ? Alignment.centerRight : null,
+      width: isCardVisibility ? 200.0.w : null,
       child: AutoSizeTexts.autoSizeText10(
+        color: isCardVisibility ? AppColors.white : Colors.grey[700],
         textOverflow: TextOverflow.fade,
         fontWeight: FontWeight.w700,
-        color: Colors.grey,
         '$city - $UF',
       ),
     );
