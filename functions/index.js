73ce7736671b9afe3a5a6b26e21d807d27749704
André Felipe <36930816+AndreDrummer@users.noreply.github.com>
2023-01-17 20:54:13 -0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,24 +7,29 @@ exports.createNotificationChat = functions.firestore
     .document('tiutiu/env/{environment}/contacts/{userId}/{contactId}/messages/{messageId}')
     .onCreate((snap, context) => {
 
+        console.log(`>> Data ${JSON.stringify(snap.data())}`);
+        console.log(`>> Params ${JSON.stringify(context.params)}`);
+        console.log(`>> Resource ${JSON.stringify(context.resource)}`);
+
         let senderParamsUid = context.params['userId']
         let senderDataUid = snap.data()['sender']['uid']
 
-        if (senderDataUid !== senderParamsUid) {
+        if(senderDataUid !== senderParamsUid) {
             admin.messaging().sendToDevice(`${snap.data()['receiver']['notificationToken']}`, {
                 notification: {
                     title: `Nova mensagem de ${snap.data()['sender']['displayName']}`,
-                    clickAction: 'FLUTTER_NOTIFICATION_CLICK',
                     body: `${snap.data()['text']}`,
                 },
                 data: {
+                    clickAction: 'FLUTTER_NOTIFICATION_CLICK',
                     data: JSON.stringify(snap.data())
                 }
             })
-        }
+        }        
     })
 
-exports.deletePostOnDennouncesLimitAchived = functions.firestore
+
+    exports.deletePostOnDennouncesLimitAchived = functions.firestore
     .document('tiutiu/env/{environment}/posts/posts/{postId}')
     .onUpdate((change, _) => {
         // Get an object representing the document
