ef60a39b12e95f6d6aea2c32c90b9690b76949ab
AndréDrummer <academicworks.adf@gmail.com>
2023-01-17 13:22:47 -0300

    turning tiutiutok screens into components

---
+++ lib/features/tiutiutok/widgets/buttons_aside.dart
@@ -0,0 +1,38 @@
+import 'package:tiutiu/features/tiutiutok/widgets/whatsapp_share_button.dart';
+import 'package:tiutiu/features/tiutiutok/widgets/disappeared_alert.dart';
+import 'package:tiutiu/features/tiutiutok/widgets/go_to_post_button.dart';
+import 'package:tiutiu/features/tiutiutok/widgets/views_count_widget.dart';
+import 'package:tiutiu/features/tiutiutok/widgets/save_button.dart';
+import 'package:tiutiu/features/tiutiutok/widgets/like_button.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/features/posts/model/post.dart';
+import 'package:flutter/material.dart';
+
+class ButtonsAside extends StatefulWidget {
+  const ButtonsAside({super.key, required this.post});
+
+  final Post post;
+
+  @override
+  State<ButtonsAside> createState() => _ButtonsAsideState();
+}
+
+class _ButtonsAsideState extends State<ButtonsAside> {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 56.0.h,
+      right: 0,
+      child: Column(
+        children: [
+          DisappearedAlertAnimation(post: widget.post),
+          ViewsCountWidget(post: widget.post),
+          LikeButton(post: widget.post),
+          SaveButton(post: widget.post),
+          WhatsAppShareButton(post: widget.post),
+          GoToPostButton(post: widget.post),
+        ],
+      ),
+    );
+  }
+}
