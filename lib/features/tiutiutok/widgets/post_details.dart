b7ed0f7712b36c9cd58e966a70f64c5e895dc602
AndréDrummer <academicworks.adf@gmail.com>
2023-01-17 13:22:47 -0300

    turning tiutiutok screens into components

---
+++ lib/features/tiutiutok/widgets/post_details.dart
@@ -0,0 +1,45 @@
+import 'package:tiutiu/core/location/models/states_and_cities.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/core/utils/other_functions.dart';
+import 'package:tiutiu/core/constants/text_styles.dart';
+import 'package:tiutiu/core/constants/app_colors.dart';
+import 'package:tiutiu/features/posts/model/post.dart';
+import 'package:tiutiu/core/pets/model/pet_model.dart';
+import 'package:tiutiu/core/utils/formatter.dart';
+import 'package:flutter/material.dart';
+
+class PostDetails extends StatelessWidget {
+  const PostDetails({super.key, required this.post});
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 96.0.h,
+      left: 16.0.w,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AutoSizeTexts.autoSizeText14(
+            fontWeight: FontWeight.w700,
+            color: AppColors.white,
+            '${Formatters.cuttedText(post.name!)} | ${(post as Pet).city} - ${StatesAndCities.stateAndCities.getInitialFromStateName(post.state)}',
+          ),
+          SizedBox(height: 4.0.h),
+          AutoSizeTexts.autoSizeText10(
+            fontWeight: FontWeight.w400,
+            color: AppColors.white,
+            '${(post as Pet).breed} - ${(post as Pet).gender} - ${(post as Pet).color}',
+          ),
+          SizedBox(height: 8.0.h),
+          AutoSizeTexts.autoSizeText10(
+            fontWeight: FontWeight.w400,
+            color: AppColors.white,
+            '${Formatters.cuttedText(OtherFunctions.replacePhoneNumberWithStars(post.description), size: 170)}',
+          ),
+        ],
+      ),
+    );
+  }
+}
