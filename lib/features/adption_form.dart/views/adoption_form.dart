4da45bdafff2098182ceb9179145ee8b76385b8c
AndréDrummer <academicworks.adf@gmail.com>
2023-01-20 23:03:50 -0300

    creating classes to adoption form

---
+++ lib/features/adption_form.dart/views/adoption_form.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/container.dart';
+
+class AdoptionForm extends StatefulWidget {
+  const AdoptionForm({super.key});
+
+  @override
+  State<AdoptionForm> createState() => _AdoptionFormState();
+}
+
+class _AdoptionFormState extends State<AdoptionForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
