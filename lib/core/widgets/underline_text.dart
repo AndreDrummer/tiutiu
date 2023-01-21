9f1d03447e7acaee7a51e5b6c97cd58ca8462b25
AndréDrummer <academicworks.adf@gmail.com>
2023-01-21 00:02:32 -0300

    implementing adoption form

--- lib/core/widgets/underline_text.dart
+++ lib/core/widgets/underline_text.dart
@@ -67,7 +67,7 @@ class UnderlineInputText extends StatelessWidget {
               '$labelText',
             ),
           ),
-          SizedBox(height: 14.0.h),
+          // SizedBox(height: 14.0.h),
           TextFormField(
             textCapitalization: capitalize ?? TextCapitalization.sentences,
             initialValue: getInitialValue(),
