a254c38709cd3c2f37bbcc228f58f1e713ad5c1f
AndréDrummer <academicworks.adf@gmail.com>
2023-01-27 19:24:50 -0300

    creating partners list

--- lib/features/sponsored/views/sponsored_horizontal_list.dart
+++ lib/features/sponsored/views/sponsored_horizontal_list.dart
@@ -1,13 +1,11 @@
+import 'package:tiutiu/features/sponsored/widget/sponsodred_tile.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:tiutiu/core/utils/launcher_functions.dart';
 import 'package:tiutiu/core/controllers/controllers.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:tiutiu/core/utils/asset_handle.dart';
 import 'package:tiutiu/core/utils/dimensions.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-class SponsoredList extends StatelessWidget {
+class SponsoredHorizontalList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double sponsoredAdsTileSize = Dimensions.getDimensBasedOnDeviceHeight(
@@ -26,28 +24,7 @@ class SponsoredList extends StatelessWidget {
       visible: visible,
       child: CarouselSlider.builder(
         itemCount: sponsoreds.length,
-        itemBuilder: (context, index, realIndex) {
-          final sponsored = sponsoreds[index];
-
-          return GestureDetector(
-            onTap: () {
-              Launcher.openBrowser(sponsored.link!);
-            },
-            child: Card(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0.h)),
-              child: Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(12.0.h)),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12.0.h),
-                  child: AssetHandle.getImage(sponsored.imagePath),
-                ),
-                alignment: Alignment.center,
-                width: Get.width,
-              ),
-              margin: EdgeInsets.only(top: 4.0.h),
-            ),
-          );
-        },
+        itemBuilder: (context, index, realIndex) => SponsoredTile(sponsored: sponsoreds[index]),
         options: CarouselOptions(
           enableInfiniteScroll: sponsoreds.length > 1,
           autoPlayCurve: Curves.easeIn,
---
+++ lib/features/sponsored/views/sponsored_vertical_list.dart
@@ -0,0 +1,46 @@
+import 'package:tiutiu/features/admob/constants/admob_block_names.dart';
+import 'package:tiutiu/features/sponsored/widget/sponsodred_tile.dart';
+import 'package:tiutiu/core/widgets/default_basic_app_bar.dart';
+import 'package:tiutiu/features/admob/widgets/ad_banner.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/core/controllers/controllers.dart';
+import 'package:tiutiu/core/widgets/one_line_text.dart';
+import 'package:tiutiu/core/constants/strings.dart';
+import 'package:flutter/material.dart';
+
+class SponsoredVerticalList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final sponsoreds = sponsoredController.sponsoreds;
+
+    return Scaffold(
+      appBar: DefaultBasicAppBar(text: MyProfileOptionsTile.partners),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: OneLineText(
+              text: 'Estes são os empreendedores que apoiam e mantêm o Tiu, tiu. Faça parte dessa lista de sucesso!',
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          Divider(),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) => SponsoredTile(sponsored: sponsoreds.reversed.toList()[index]),
+              padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+              itemCount: sponsoreds.length,
+            ),
+          ),
+          AdBanner(
+            adId: systemController.getAdMobBlockID(
+              blockName: AdMobBlockName.homeFooterAdBlockId,
+              type: AdMobType.banner,
+            ),
+          ),
+          SizedBox(height: 1.0.h),
+        ],
+      ),
+    );
+  }
+}
