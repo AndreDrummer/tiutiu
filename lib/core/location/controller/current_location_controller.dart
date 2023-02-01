6ffa5f6f5345c15d84f8f266037e5b207047d0c7
AndréDrummer <academicworks.adf@gmail.com>
2023-02-01 00:59:46 -0300

    fixing issues required by apple

--- lib/core/location/controller/current_location_controller.dart
+++ lib/core/location/controller/current_location_controller.dart
@@ -1,4 +1,3 @@
-import 'package:tiutiu/core/extensions/service_location_status.dart';
 import 'package:tiutiu/core/local_storage/local_storage_keys.dart';
 import 'package:tiutiu/core/extensions/string_extension.dart';
 import 'package:tiutiu/core/local_storage/local_storage.dart';
@@ -12,7 +11,6 @@ import 'dart:io';
 
 class CurrentLocationController extends GetxController {
   final Rx<LocationPermission> _permission = LocationPermission.unableToDetermine.obs;
-  final Rx<GPSStatus> _gpsStatus = GPSStatus.deactivated.obs;
   final Rx<Placemark> _currentPlacemark = Placemark().obs;
   final Rx<LatLng> _currentLocation = LatLng(0, 0).obs;
   final RxBool _isPermissionGranted = false.obs;
@@ -22,7 +20,6 @@ class CurrentLocationController extends GetxController {
   Placemark get currentPlacemark => _currentPlacemark.value;
   LocationPermission get permission => _permission.value;
   LatLng get location => _currentLocation.value;
-  GPSStatus get gpsStatus => _gpsStatus.value;
   bool get canContinue => _canContinue.value;
 
   void set currentPlacemark(Placemark placemark) {
@@ -31,10 +28,6 @@ class CurrentLocationController extends GetxController {
 
   void set permission(LocationPermission value) {
     _permission(value);
-  }
-
-  void set locationServiceEnabled(GPSStatus status) {
-    _gpsStatus(status);
   }
 
   void set location(LatLng value) {
@@ -49,18 +42,6 @@ class CurrentLocationController extends GetxController {
     _canContinue(value);
   }
 
-  Future<void> updateGPSStatus() async {
-    final isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
-
-    if (isLocationServiceEnabled) {
-      _gpsStatus(GPSStatus.active);
-      if (kDebugMode) debugPrint('TiuTiuApp: GPS was activated');
-    } else {
-      _gpsStatus(GPSStatus.deactivated);
-      if (kDebugMode) debugPrint('TiuTiuApp: GPS was NOT activated');
-    }
-  }
-
   Future<void> checkPermission() async {
     permission = await Geolocator.checkPermission();
 
@@ -72,24 +53,11 @@ class CurrentLocationController extends GetxController {
   Future<void> updatePermission() async {
     permission = await Geolocator.requestPermission();
     setUserLocation();
+
     if (kDebugMode) debugPrint('TiuTiuApp: local access permission $permission');
   }
 
-  Future<void> handleLocationPermission() async {
-    final permissionDeniedForever = permission == LocationPermission.deniedForever;
-
-    if (permissionDeniedForever) {
-      if (kDebugMode) debugPrint('TiuTiuApp: local access denied forever? $permissionDeniedForever');
-      await Geolocator.openAppSettings();
-    } else {
-      updatePermission();
-    }
-  }
-
-  Future<void> openDeviceSettings() async {
-    await Geolocator.openLocationSettings();
-    await updateGPSStatus();
-  }
+  Future<void> openDeviceSettings() async => await Geolocator.openLocationSettings();
 
   Future<void> setUserLocation({LatLng? currentLocation}) async {
     await checkPermission();
--- lib/core/location/views/localization_service_access_permission_request.dart
+++ lib/core/location/views/localization_service_access_permission_request.dart
@@ -1,114 +1,147 @@
 import 'package:tiutiu/core/widgets/default_basic_app_bar.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiutiu/core/widgets/lottie_animation.dart';
 import 'package:tiutiu/core/controllers/controllers.dart';
 import 'package:tiutiu/core/widgets/app_name_widget.dart';
+import 'package:tiutiu/core/widgets/async_handler.dart';
+import 'package:tiutiu/core/mixins/tiu_tiu_pop_up.dart';
 import 'package:tiutiu/core/constants/assets_path.dart';
 import 'package:tiutiu/core/constants/text_styles.dart';
 import 'package:tiutiu/core/constants/app_colors.dart';
 import 'package:tiutiu/core/widgets/button_wide.dart';
 import 'package:tiutiu/core/widgets/background.dart';
 import 'package:tiutiu/core/constants/strings.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'dart:io';
 
-class LocalizationServiceAccessPermissionAccess extends StatelessWidget {
+class LocalizationServiceAccessPermissionAccess extends StatefulWidget {
   const LocalizationServiceAccessPermissionAccess({
+    this.requestPermissionOnInit = false,
     this.localAccessDenied = false,
-    this.showSpyButton = true,
-    this.gpsIsActive = false,
+    this.isInPostScreen = false,
     super.key,
   });
 
+  final bool requestPermissionOnInit;
   final bool localAccessDenied;
-  final bool showSpyButton;
-  final bool gpsIsActive;
+  final bool isInPostScreen;
+
+  @override
+  State<LocalizationServiceAccessPermissionAccess> createState() => _LocalizationServiceAccessPermissionAccessState();
+}
+
+class _LocalizationServiceAccessPermissionAccessState extends State<LocalizationServiceAccessPermissionAccess>
+    with TiuTiuPopUp {
+  final ValueNotifier<PermissionStatus> locationAccessStatus =
+      ValueNotifier<PermissionStatus>(PermissionStatus.limited);
+  bool get requestPermissionOnInit => widget.requestPermissionOnInit;
+  bool get isInPostScreen => widget.isInPostScreen;
+
+  Future<PermissionStatus> getPermissionStatus() async {
+    if (requestPermissionOnInit) {
+      return Permission.location.request();
+    }
+    return Permission.location.status;
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (kDebugMode) debugPrint('TiuTiuApp: local access denied? ${localAccessDenied ? 'Sim' : 'Não'}');
+    if (kDebugMode) debugPrint('TiuTiuApp: local access denied? ${widget.localAccessDenied ? 'Sim' : 'Não'}');
 
     return Scaffold(
-        appBar: DefaultBasicAppBar(text: LocalPermissionStrings.appBarTitle),
-        body: Center(
-          child: Column(
-            children: [
-              _petPawPin(),
-              SizedBox(height: 32.0.h),
-              _googleRoutesImage(),
-              SizedBox(height: Get.width / 8),
-              AppNameWidget(),
-              SizedBox(height: 8.0.h),
-              _explainAccessPermissionText(),
-              SizedBox(height: 8.0.h),
-              _warningAboutConfigsSeetings(),
-              Spacer(),
-              _primaryButton(),
-              Spacer(),
-            ],
-          ),
-        ));
-  }
+      appBar: DefaultBasicAppBar(text: LocalPermissionStrings.appBarTitle),
+      body: FutureBuilder<PermissionStatus>(
+        future: getPermissionStatus(),
+        builder: (context, snapshot) {
+          return AsyncHandler<PermissionStatus>(
+            snapshot: snapshot,
+            buildWidget: (status) {
+              locationAccessStatus.value = status;
 
-  Widget _petPawPin() {
-    return LottieAnimation(animationPath: AnimationsAssets.petLocationPin, size: 120.0.h);
-  }
-
-  Background _googleRoutesImage() => Background(image: ImageAssets.googlePlaces);
-
-  Widget _warningAboutConfigsSeetings() {
-    return Visibility(
-      visible: currentLocationController.permission == LocationPermission.deniedForever && Platform.isAndroid,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8.0.w),
-        child: AutoSizeTexts.autoSizeText14(
-          LocalPermissionStrings.permissionDeniedForeverWarning,
-          textAlign: TextAlign.center,
-          color: AppColors.danger,
-        ),
+              return Center(
+                child: Column(
+                  children: [
+                    _petPawPin(),
+                    SizedBox(height: 32.0.h),
+                    _googleRoutesImage(),
+                    SizedBox(height: Get.width / 8),
+                    AppNameWidget(),
+                    SizedBox(height: 8.0.h),
+                    _explainAccessPermissionText(),
+                    Spacer(),
+                    _primaryButton(),
+                    Spacer(),
+                  ],
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
+
+  Widget _petPawPin() => LottieAnimation(animationPath: AnimationsAssets.petLocationPin, size: 120.0.h);
+
+  Background _googleRoutesImage() => Background(image: ImageAssets.googlePlaces);
 
   Widget _explainAccessPermissionText() {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 8.0.w),
       child: AutoSizeTexts.autoSizeText16(
-        gpsIsActive ? LocalPermissionStrings.needsAccess : LocalPermissionStrings.needsGPS,
+        LocalPermissionStrings.needsAccess,
         textAlign: TextAlign.center,
       ),
     );
   }
 
-  ButtonWide _primaryButton() {
-    return ButtonWide(
-      onPressed: onPrimaryPressed,
-      text: _getButtonText(),
+  Widget _primaryButton() {
+    return ValueListenableBuilder(
+      valueListenable: locationAccessStatus,
+      builder: (context, value, child) {
+        String? buttonText;
+
+        if (locationAccessStatus.value == PermissionStatus.permanentlyDenied) {
+          buttonText = AppStrings.contines;
+        } else if (locationAccessStatus.value == PermissionStatus.denied) {
+          buttonText = LocalPermissionStrings.grantAcess;
+        }
+
+        return ButtonWide(onPressed: () async => onPrimaryPressed(), text: buttonText);
+      },
     );
   }
 
-  void onPrimaryPressed() {
-    if (!gpsIsActive || currentLocationController.permission == LocationPermission.deniedForever) {
-      currentLocationController.openDeviceSettings();
+  Future<void> onPrimaryPressed() async {
+    print(locationAccessStatus.value);
+    if (locationAccessStatus.value == PermissionStatus.permanentlyDenied) {
+      await showWarningPopup();
     } else {
-      currentLocationController.handleLocationPermission();
+      locationAccessStatus.value = await Permission.location.request();
+      if (locationAccessStatus.value == PermissionStatus.permanentlyDenied) {
+        await showWarningPopup();
+      }
     }
   }
 
-  String _getButtonText() {
-    if (!gpsIsActive) {
-      return LocalPermissionStrings.turnOnLocalization;
-    } else {
-      if (currentLocationController.permission == LocationPermission.deniedForever) {
-        return LocalPermissionStrings.openSettings;
-      } else if (currentLocationController.permission == LocationPermission.denied) {
-        return LocalPermissionStrings.grantAcess;
-      }
-
-      return LocalPermissionStrings.grantAcess;
-    }
+  Future<void> showWarningPopup() async {
+    return await showPopUp(
+      message:
+          'O Tiutiu funciona melhor com o acesso a localização. Você até pode entrar e ver a lista de PETS, mas se for fazer uma publicação, vai precisar autorizar o acesso a sua localização!\n\nO que você quer fazer?',
+      confirmText: 'Abrir configurações e conceder o acesso',
+      title: LocalPermissionStrings.localization,
+      denyText: isInPostScreen ? 'Não quero postar agora' : 'Continuar sem dar acesso',
+      secondaryAction: () {
+        Get.back();
+        currentLocationController.openDeviceSettings();
+      },
+      mainAction: () {
+        currentLocationController.canContinue = true;
+        Get.back();
+      },
+      backGroundColor: AppColors.info,
+    );
   }
 }
