dfc9c1b332dd84dd364fdd52e1b4c85e8d565e59
AndréDrummer <academicworks.adf@gmail.com>
2023-02-01 19:57:09 -0300

    stablishing a wpp initial message, fixing like action behavior and updating location permission requests

--- lib/core/system/app_bootstrap.dart
+++ lib/core/system/app_bootstrap.dart
@@ -1,7 +1,7 @@
 import 'package:tiutiu/core/location/views/localization_service_access_permission_request.dart';
 import 'package:tiutiu/features/auth/views/auth_or_home.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:tiutiu/core/controllers/controllers.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -26,10 +26,6 @@ class _BootstrapState extends State<AppBootstrap> {
     );
   }
 
-  bool isLocalAccessPermissionDenied(
-    LocationPermission currentLocationPermission,
-  ) {
-    return currentLocationPermission != LocationPermission.always &&
-        currentLocationPermission != LocationPermission.whileInUse;
-  }
+  bool isLocalAccessPermissionDenied(PermissionStatus currentLocationPermission) =>
+      currentLocationPermission != PermissionStatus.granted;
 }
