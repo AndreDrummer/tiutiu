bfdeb610791581d688fce9b4ba2f7f4780426180
AndréDrummer <academicworks.adf@gmail.com>
2023-02-20 18:55:19 -0300

    finishing translation

--- lib/core/widgets/hint_error.dart
+++ lib/core/widgets/hint_error.dart
@@ -1,10 +1,11 @@
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiutiu/core/constants/app_colors.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class HintError extends StatelessWidget {
-  HintError({this.message = 'Campo obrigatório.'});
+  HintError({this.message});
 
   final message;
 
@@ -15,7 +16,7 @@ class HintError extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.only(top: 8.0.h),
         child: AutoSizeText(
-          message,
+          message ?? AppLocalizations.of(context).requiredField,
           style: TextStyle(
             fontWeight: FontWeight.w400,
             color: AppColors.danger,
