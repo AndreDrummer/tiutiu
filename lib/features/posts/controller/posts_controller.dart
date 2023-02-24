9102b1c50ef4ebee7ea5f378fc85bf98ad80ae58
AndréDrummer <academicworks.adf@gmail.com>
2023-02-24 20:03:11 -0300

    setting country on a post

--- lib/features/posts/controller/posts_controller.dart
+++ lib/features/posts/controller/posts_controller.dart
@@ -437,6 +437,7 @@ class PostsController extends GetxController with TiuTiuPopUp {
       city = 'Acrelândia';
     }
 
+    postMap[PostEnum.country.name] = systemController.properties.userChoiceCountry;
     postMap[PostEnum.state.name] = state;
     postMap[PostEnum.city.name] = city;
   }
