a7cd3bfd6955dd03c22f260b861232ac29fa2957
AndréDrummer <academicworks.adf@gmail.com>
2023-03-14 23:19:41 -0300

    fixing filters on top

--- lib/features/home/widgets/persistent_header_deleagate.dart
+++ lib/features/home/widgets/persistent_header_deleagate.dart
@@ -1,38 +1,47 @@
+import 'package:tiutiu/features/posts/widgets/filter_count_order_by.dart';
+import 'package:tiutiu/features/home/controller/home_controller.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tiutiu/core/controllers/controllers.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tiutiu/core/controllers/controllers.dart';
-import 'package:tiutiu/features/posts/widgets/filter_count_order_by.dart';
-
 import 'filters_type.dart';
 
 class PersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
-
-  PersistentHeaderDelegate({required this.heigthExtent});
-  
-  final double heigthExtent;
-
   @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      padding: EdgeInsets.only(
-        top: 10,
-      ),
-      decoration: BoxDecoration(color: Colors.transparent),
-      child: Column(
-        children: [
-         FiltersType(),
-          Obx(() => FilterResultCount(postsCount: postsController.postsCount))
-        ],
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Visibility(
+      visible: homeController.bottomBarIndex == BottomBarIndex.DONATE.indx ||
+          homeController.bottomBarIndex == BottomBarIndex.FINDER.indx,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        padding: EdgeInsets.only(top: 4.0.h),
+        child: ListView(
+          children: [
+            FiltersType(),
+            Obx(
+              () => Container(
+                margin: EdgeInsets.only(top: 4.0.h),
+                child: FilterResultCount(postsCount: postsController.postsCount),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  @override
-  double get maxExtent => heigthExtent;
+  final extent = homeController.bottomBarIndex == BottomBarIndex.DONATE.indx ||
+          homeController.bottomBarIndex == BottomBarIndex.FINDER.indx
+      ? 102.0.h
+      : 0.0.h;
 
   @override
-  double get minExtent => heigthExtent;
+  double get maxExtent => extent;
+
+  @override
+  double get minExtent => extent;
 
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
