aa9acf3a06e66fd60e3d8b86da0d3514bf8f1e6e
AndréDrummer <academicworks.adf@gmail.com>
2023-03-15 15:10:03 -0300

    fixing filters on top and adjusting size for all devices

--- lib/features/home/utils/expanded_home_height_size.dart
+++ lib/features/home/utils/expanded_home_height_size.dart
@@ -3,19 +3,20 @@ import 'package:get/get.dart';
 
 double expandedHomeHeightDefault({required bool showingSponsoredAds}) {
   return Dimensions.getDimensBasedOnDeviceHeight(
-    smaller: showingSponsoredAds ? Get.height / 5.0 : Get.width / 5.0,
+    smaller: showingSponsoredAds ? Get.height / 5.0 : Get.width / 6.0,
     medium: showingSponsoredAds ? Get.height / 6.0 : Get.width / 6.0,
     bigger: showingSponsoredAds ? Get.height / 6.0 : Get.width / 6.0,
+    tablet: showingSponsoredAds ? Get.height / 5.0 : Get.width / 7.5,
   );
 }
 
 double expandedHomeHeightWithAdminInfoAndInternetConnection({required bool showingSponsoredAds}) {
   return Dimensions.getDimensBasedOnDeviceHeight(
     xBigger: showingSponsoredAds ? Get.height / 2.40 : Get.height / 3.40,
-    xSmaller: showingSponsoredAds ? Get.height / 2.25 : Get.height / 3.1,
-    smaller: showingSponsoredAds ? Get.height / 2.35 : Get.height / 3.2,
-    medium: showingSponsoredAds ? Get.height / 2.70 : Get.height / 3.70,
-    bigger: showingSponsoredAds ? Get.height / 4.5 : Get.width / 4.0,
+    smaller: showingSponsoredAds ? Get.height / 3.70 : Get.width / 3.80,
+    medium: showingSponsoredAds ? Get.height / 4.70 : Get.width / 3.80,
+    bigger: showingSponsoredAds ? Get.height / 4.50 : Get.width / 4.0,
+    tablet: showingSponsoredAds ? Get.height / 4.40 : Get.width / 5.0,
   );
 }
 
