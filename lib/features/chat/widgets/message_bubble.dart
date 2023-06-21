e78e28fc299ada9bdb93a2b82b6c045f2f46848a
AndréDrummer <academicworks.adf@gmail.com>
2023-06-20 22:30:38 -0300

    upgrading dart sdk

--- lib/features/chat/widgets/message_bubble.dart
+++ lib/features/chat/widgets/message_bubble.dart
@@ -1,8 +1,6 @@
-import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_1.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiutiu/core/constants/text_styles.dart';
 import 'package:tiutiu/core/constants/app_colors.dart';
-import 'package:flutter_chat_bubble/bubble_type.dart';
 import 'package:flutter_chat_bubble/chat_bubble.dart';
 import 'package:tiutiu/core/utils/formatter.dart';
 import 'package:flutter/material.dart';
