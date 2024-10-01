67ca711d16f31d0d47b84d9ff92fc71536af6cf5
André Felipe <academicworks.adf@gmail.com>
2024-10-01 17:48:06 -0300

    setting up ImageNotification

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by André Felipe Silva on 01/10/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
