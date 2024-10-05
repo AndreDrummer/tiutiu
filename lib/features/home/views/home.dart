c20d113ca554a204363e1fd42bfda0de28fd2f86
André Felipe <academicworks.adf@gmail.com>
2024-10-05 20:18:18 -0300

    requesting notifications

--- lib/features/home/views/home.dart
+++ lib/features/home/views/home.dart
@@ -13,8 +13,8 @@ import 'package:tiutiu/features/home/widgets/bottom_bar.dart';
 import 'package:tiutiu/features/admob/widgets/ad_banner.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:tiutiu/features/posts/views/posts_list.dart';
-import 'package:move_to_background/move_to_background.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:tiutiu/core/controllers/controllers.dart';
 import 'package:tiutiu/core/mixins/tiu_tiu_pop_up.dart';
 import 'package:tiutiu/core/constants/app_colors.dart';
@@ -22,9 +22,9 @@ import 'package:tiutiu/features/shop/tiutiushop.dart';
 import 'package:tiutiu/features/more/views/more.dart';
 import 'package:tiutiu/core/utils/dimensions.dart';
 import 'package:tiutiu/core/widgets/top_bar.dart';
+import 'package:open_settings/open_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'dart:io';
 
 class Home extends StatefulWidget {
   @override
@@ -38,9 +38,45 @@ class _HomeState extends State<Home> with TiuTiuPopUp {
     super.initState();
   }
 
+  Future<void> _promptRequestToNotification() async {
+    showPopUp(
+      message: AppLocalizations.of(Get.context!)!.notificationsMessageRequest,
+      backGroundColor: AppColors.info,
+      confirmText: AppLocalizations.of(Get.context!)!.yes,
+      title: AppLocalizations.of(Get.context!)!.notificationsWarning,
+      barrierDismissible: false,
+      denyText: AppLocalizations.of(Get.context!)!.no,
+      mainAction: () {
+        Get.back();
+      },
+      secondaryAction: () {
+        OpenSettings.openAppNotificationSetting();
+        Get.back();
+      },
+    );
+  }
+
+  Future<void> handleNotificationsDeniedRequests() async {
+    final notificationPermissionStatus = await Permission.notification.status;
+
+    print("Notifications status $notificationPermissionStatus");
+
+    if (notificationPermissionStatus == PermissionStatus.denied) {
+      await Permission.notification.request();
+    } else if (notificationPermissionStatus == PermissionStatus.permanentlyDenied) {
+      await _promptRequestToNotification();
+    }
+
+    print("Notifications status $notificationPermissionStatus");
+  }
+
   Future<void> showAdAndRequestToTrack() async {
-    await adMobController.showOpeningAd();
-    await crashlyticsController.init();
+    Future.delayed(Duration(seconds: 1), () async {
+      await adMobController.showOpeningAd();
+      await crashlyticsController.init();
+
+      await handleNotificationsDeniedRequests();
+    });
   }
 
   @override
@@ -49,8 +85,7 @@ class _HomeState extends State<Home> with TiuTiuPopUp {
       child: Obx(
         () {
           final conditionToShowFloatingButton =
-              homeController.bottomBarIndex == BottomBarIndex.DONATE.indx &&
-                  !systemController.isToCloseApp;
+              homeController.bottomBarIndex == BottomBarIndex.DONATE.indx && !systemController.isToCloseApp;
 
           final screens = <Widget>[
             ClosedForMaintenance(child: DonateList()),
@@ -62,112 +97,77 @@ class _HomeState extends State<Home> with TiuTiuPopUp {
             ClosedForMaintenance(child: More()),
           ];
 
-          return PopScope(
-            onPopInvokedWithResult: (_, __) async {
-              bool willClose = false;
-
-              return showPopUp(
-                backGroundColor: AppColors.warning,
-                message: AppLocalizations.of(context)!.wannaLeave,
-                confirmText: AppLocalizations.of(context)!.yes,
-                textColor: AppColors.black,
-                barrierDismissible: false,
-                title: AppLocalizations.of(context)!.endApp,
-                denyText: AppLocalizations.of(context)!.no,
-                mainAction: () {
-                  Get.back();
-                },
-                secondaryAction: () {
-                  if (Platform.isIOS) {
-                    willClose = true;
-                  } else {
-                    MoveToBackground.moveTaskToBack();
-                  }
-                  Get.back();
-                },
-              ).then((_) => willClose);
-            },
-            child: Scaffold(
-              restorationId: 'scaffold',
-              key: UniqueKey(),
-              body: NestedScrollView(
-                headerSliverBuilder: (context, innerBoxIsScrolled) {
-                  return [
-                    Obx(
-                      () {
-                        return SliverAppBar(
-                          toolbarHeight: conditionToShowFloatingButton &&
-                                  !systemController.properties.internetConnected
-                              ? Dimensions.getDimensBasedOnDeviceHeight(
-                                  smaller: 92.0.h,
-                                  medium: 84.0.h,
-                                  bigger: 76.0.h,
-                                )
-                              : toolbarHeight(),
-                          backgroundColor: Colors.transparent,
-                          automaticallyImplyLeading: false,
-                          expandedHeight: expandedHeight(),
-                          shadowColor: AppColors.white,
-                          flexibleSpace: TopBar(),
-                          floating: true,
-                          pinned: true,
-                        );
-                      },
-                    ),
-                    SliverPersistentHeader(
-                      floating: true,
-                      pinned: true,
-                      delegate: PersistentHeaderDelegate(),
-                    ),
-                  ];
-                },
-                body: Stack(
-                  children: [
-                    Positioned.fill(
-                        child:
-                            screens.elementAt(homeController.bottomBarIndex)),
-                    Visibility(
-                      visible: homeController.bottomBarIndex ==
-                              BottomBarIndex.TIUTIUTOK.indx
-                          ? postsController.tiutiutokPostsListIsEmpty
-                          : true,
-                      child: Positioned(
-                        child: AdBanner(
-                          adId: systemController.getAdMobBlockID(
-                            blockName: AdMobBlockName.homeFooterAdBlockId,
-                            type: AdMobType.banner,
-                          ),
+          return Scaffold(
+            restorationId: 'scaffold',
+            key: UniqueKey(),
+            body: NestedScrollView(
+              headerSliverBuilder: (context, innerBoxIsScrolled) {
+                return [
+                  Obx(
+                    () {
+                      return SliverAppBar(
+                        toolbarHeight: conditionToShowFloatingButton && !systemController.properties.internetConnected
+                            ? Dimensions.getDimensBasedOnDeviceHeight(
+                                smaller: 92.0.h,
+                                medium: 84.0.h,
+                                bigger: 76.0.h,
+                              )
+                            : toolbarHeight(),
+                        backgroundColor: Colors.transparent,
+                        automaticallyImplyLeading: false,
+                        expandedHeight: expandedHeight(),
+                        shadowColor: AppColors.white,
+                        flexibleSpace: TopBar(),
+                        floating: true,
+                        pinned: true,
+                      );
+                    },
+                  ),
+                  SliverPersistentHeader(
+                    floating: true,
+                    pinned: true,
+                    delegate: PersistentHeaderDelegate(),
+                  ),
+                ];
+              },
+              body: Stack(
+                children: [
+                  Positioned.fill(child: screens.elementAt(homeController.bottomBarIndex)),
+                  Visibility(
+                    visible: homeController.bottomBarIndex == BottomBarIndex.TIUTIUTOK.indx
+                        ? postsController.tiutiutokPostsListIsEmpty
+                        : true,
+                    child: Positioned(
+                      child: AdBanner(
+                        adId: systemController.getAdMobBlockID(
+                          blockName: AdMobBlockName.homeFooterAdBlockId,
+                          type: AdMobType.banner,
                         ),
-                        bottom: 56,
-                        right: 0,
-                        left: 0,
                       ),
-                    ),
-                    Positioned(
-                      child: BottomBar(),
-                      bottom: 0,
+                      bottom: 56,
                       right: 0,
                       left: 0,
                     ),
-                  ],
-                ),
-                controller: homeController.scrollController,
-                floatHeaderSlivers: true,
+                  ),
+                  Positioned(
+                    child: BottomBar(),
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                  ),
+                ],
               ),
-              floatingActionButton: Padding(
-                padding: EdgeInsets.only(
-                    bottom: adminRemoteConfigController.configs.allowGoogleAds
-                        ? 88.0.h
-                        : 56.0.h),
-                child: ChangePostsVisibilityFloatingButtom(
-                  visibility: conditionToShowFloatingButton &&
-                      postsController.filteredPosts.isNotEmpty,
-                ),
-              ),
-              floatingActionButtonLocation:
-                  FloatingActionButtonLocation.miniEndFloat,
-              resizeToAvoidBottomInset: false,
+              controller: homeController.scrollController,
+              floatHeaderSlivers: true,
             ),
+            floatingActionButton: Padding(
+              padding: EdgeInsets.only(bottom: adminRemoteConfigController.configs.allowGoogleAds ? 88.0.h : 56.0.h),
+              child: ChangePostsVisibilityFloatingButtom(
+                visibility: conditionToShowFloatingButton && postsController.filteredPosts.isNotEmpty,
+              ),
+            ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+            resizeToAvoidBottomInset: false,
           );
         },
       ),
@@ -178,10 +178,8 @@ class _HomeState extends State<Home> with TiuTiuPopUp {
     final AdminRemoteConfig configs = adminRemoteConfigController.configs;
     final List<Sponsored> sponsoreds = sponsoredController.sponsoreds;
 
-    final showingSponsoredAds =
-        configs.showSponsoredAds && sponsoreds.isNotEmpty;
-    final isFilteringByName = filterController.filterParams.value.orderBy ==
-        AppLocalizations.of(context)!.name;
+    final showingSponsoredAds = configs.showSponsoredAds && sponsoreds.isNotEmpty;
+    final isFilteringByName = filterController.filterParams.value.orderBy == AppLocalizations.of(context)!.name;
     final appIsClosed = systemController.isToCloseApp;
 
     final conditionToAddHeight = !appIsClosed &&
@@ -190,14 +188,11 @@ class _HomeState extends State<Home> with TiuTiuPopUp {
                 homeController.bottomBarIndex == BottomBarIndex.FINDER.indx));
 
     if (conditionToAddHeight) {
-      final thereIsDeveloperCommunication =
-          adminRemoteConfigController.configs.thereIsAdminCommunication;
+      final thereIsDeveloperCommunication = adminRemoteConfigController.configs.thereIsAdminCommunication;
 
-      final showInfoBanner = (authController.userExists &&
-          !tiutiuUserController.tiutiuUser.emailVerified);
+      final showInfoBanner = (authController.userExists && !tiutiuUserController.tiutiuUser.emailVerified);
 
-      if ((thereIsDeveloperCommunication || showInfoBanner) &&
-          systemController.properties.internetConnected) {
+      if ((thereIsDeveloperCommunication || showInfoBanner) && systemController.properties.internetConnected) {
         return expandedHomeHeightWithAdminInfoAndInternetConnection(
           showingSponsoredAds: showingSponsoredAds || isFilteringByName,
         );
@@ -206,8 +201,7 @@ class _HomeState extends State<Home> with TiuTiuPopUp {
           showingSponsoredAds: showingSponsoredAds || isFilteringByName,
         );
       } else {
-        return expandedHomeHeightDefault(
-            showingSponsoredAds: showingSponsoredAds || isFilteringByName);
+        return expandedHomeHeightDefault(showingSponsoredAds: showingSponsoredAds || isFilteringByName);
       }
     }
 
@@ -220,10 +214,8 @@ class _HomeState extends State<Home> with TiuTiuPopUp {
 
     if (homeController.bottomBarIndex != BottomBarIndex.DONATE.indx) return 0.0;
 
-    if (cardVisibilityKind == CardVisibilityKind.banner && posts.length < 6)
-      return 56.0;
-    if (cardVisibilityKind == CardVisibilityKind.card && posts.length < 3)
-      return 56.0;
+    if (cardVisibilityKind == CardVisibilityKind.banner && posts.length < 6) return 56.0;
+    if (cardVisibilityKind == CardVisibilityKind.card && posts.length < 3) return 56.0;
 
     return 0.0;
   }
